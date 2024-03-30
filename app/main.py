2544586fc04854e865007ad8946ed8fab35973e3
Sikach Illia <55293120+isikach@users.noreply.github.com>
2024-03-30 23:29:04 +0200

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,11 +1,12 @@
 import os
+
 import requests
 
 
 API_KEY = os.getenv("API_KEY")
 CITY = "Paris"
 payload = {"key": API_KEY, "q": CITY}
-URL = f"http://api.weatherapi.com/v1/current.json"
+URL = "http://api.weatherapi.com/v1/current.json"
 
 
 def get_weather() -> None:
@@ -16,11 +17,14 @@ def get_weather() -> None:
         local_time = data["location"]["localtime"]
         celsius = data["current"]["temp_c"]
         condition = data["current"]["condition"]["text"]
-        print(f"In {CITY}/{county_name} {local_time} "
-           f"Weather {celsius} Celsius, {condition}")
+        print(
+            f"In {CITY}/{county_name} {local_time} "
+            f"Weather {celsius} Celsius, {condition}"
+        )
     except requests.exceptions.ConnectionError:
         print("Connection")
 
 
 if __name__ == "__main__":
     get_weather()
+
