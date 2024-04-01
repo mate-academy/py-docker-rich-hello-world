c9d2cffe81a73e874bfdbcaf04c7bc0ec3533a77
Yevhen <evgeniy.playid@icloud.com>
2024-04-01 17:33:37 +0300

    Add errors handling

--- app/main.py
+++ app/main.py
@@ -18,28 +18,32 @@ def get_weather() -> None:
         "q": CITY,
     }
 
-    response = requests.get(URL, params=response_params)
-    response.raise_for_status()  # Raise an exception for unsuccessful response
-    weather_data = response.json()
+    try:
+        response = requests.get(URL, params=response_params)
+        response.raise_for_status()
+        weather_data = response.json()
 
-    city = weather_data.get(
-        "location", {}).get("name", "Unknown city")
-    country = weather_data.get(
-        "location", {}).get("country", "Unknown country")
-    local_time = weather_data.get(
-        "location", {}).get("localtime", "Unknown time")
-    temp_c = weather_data.get(
-        "current", {}).get("temp_c", "value not correct")
-    condition_text = (
-        weather_data.get("current", {})
-        .get("condition", {})
-        .get("text", "value not correct")
-    )
+        city = weather_data.get(
+            "location", {}).get("name", "Unknown city")
+        country = weather_data.get(
+            "location", {}).get("country", "Unknown country")
+        local_time = weather_data.get(
+            "location", {}).get("localtime", "Unknown time")
+        temp_c = weather_data.get(
+            "current", {}).get("temp_c", "value not correct")
+        condition_text = (
+            weather_data.get("current", {})
+            .get("condition", {})
+            .get("text", "value not correct")
+        )
 
-    print(
-        f"Weather in {city},{country} is {condition_text} at {local_time}."
-        f"Air temperature: {temp_c} degrees Celsius"
-    )
+        print(
+            f"Weather in {city},{country} is {condition_text} at {local_time}."
+            f"Air temperature: {temp_c} degrees Celsius"
+        )
+
+    except Exception as e:
+        print(f"Error occurred: {e}")
 
 
 if __name__ == "__main__":
