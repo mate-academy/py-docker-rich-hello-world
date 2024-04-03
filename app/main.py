ff83764a3442c65620a0230d82fec0f623fd10da
Ihor Bondarenko <igorb6766@gmail.com>
2024-04-03 19:17:01 +0300

    update get_weather func

--- app/main.py
+++ app/main.py
@@ -2,31 +2,39 @@ import os
 import requests
 
 API_KEY = os.environ.get("WEATHER_API_KEY")
-BASE_URL = "http://api.weatherapi.com/v1/"
+BASE_URL = "http://api.weatherapi.com/v1/current.json"
 CITY = "Paris"
 
 
-def get_weather(api_key: str) -> dict:
-    url = BASE_URL + f"current.json?key={api_key}&q={CITY}"
-    response = requests.get(url)
-    data = response.json()
-    return data["current"]
+def get_weather() -> dict | None:
+    payload = {
+        "q": CITY,
+        "key": API_KEY
+    }
+    response = requests.get(BASE_URL, params=payload)
+
+    if response.status_code == 200:
+        return response.json()["current"]
+
+    print(f"Error: Failed to fetch weather data. "
+          f"Status code: {response.status_code}")
+
+    return None
 
 
 def print_weather(weather: dict) -> None:
     print("Weather in Paris:")
-    print(f"Temperature: {weather['temp_c']}°C")
-    print(f"Condition: {weather['condition']['text']}")
-    print(f"Wind: {weather['wind_kph']} km/h")
+    print(f"Temperature: {weather["temp_c"]}°C")
+    print(f"Condition: {weather["condition"]["text"]}")
+    print(f"Wind: {weather["wind_kph"]} km/h")
 
 
 if __name__ == "__main__":
-    api_key = API_KEY
-    if not api_key:
+    if not API_KEY:
         print(
             "Error: Weather API key is missing. "
             "Set WEATHER_API_KEY environment variable."
         )
     else:
-        weather = get_weather(api_key)
+        weather = get_weather()
         print_weather(weather)
