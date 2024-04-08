236040e4576bb7727f267a780ef44c1d045425df
Molchanov Ihor <IMolchanov@kdmg.com.ua>
2024-04-08 15:33:17 +0300

    init developer branch, add api script

---
+++ Dockerfile
@@ -0,0 +1,12 @@
+FROM python:3.11
+LABEL maintainer="adonos90@gmail.com"
+ENV PYTHOUNNBUFFERED 1
+
+WORKDIR app/
+
+COPY requirements.txt requirements.txt
+RUN pip install -r requirements.txt
+
+COPY . .
+
+CMD["python", "manage.py", "runserver", "0.0.0.0:80"]
--- app/main.py
+++ app/main.py
@@ -1,6 +1,23 @@
+import requests
+
+API_KEY = "be47c9b11f124244959115558240804"
+
+
 def get_weather() -> None:
-    # write your code here
-    pass
+    CITY = "Paris"
+    URL = f"http://api.weatherapi.com/v1/current.json?"
+    response = requests.get(f"{URL}key={API_KEY}&q={CITY}")
+    if response.status_code == 200:
+        data = response.json()
+        temp_c = data["current"]["temp_c"]
+        temp_f = data["current"]["temp_f"]
+        wind_speed_km = data["current"]["wind_kph"]
+        wind_speed_mile = data["current"]["wind_mph"]
+        print(f"City: {CITY}\n"
+              f"Temperature: {temp_c} °C ({temp_f} °F)\n"
+              f"Wind Speed: {wind_speed_km} km ({wind_speed_mile} mile)")
+        return
+    print(f"Could not get weather, status code: {response.status_code}")
 
 
 if __name__ == "__main__":
