c4bfc3d18cc776ecfcaaa5a25091f0885a38822b
Ant <u123@ua.fm>
2024-04-12 12:23:07 +0300

    Prepare docker image

--- app/main.py
+++ app/main.py
@@ -1,6 +1,29 @@
+import os
+
+import requests
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+API_KEY = os.getenv("API_KEY")
+REGION = "paris"
+URL = "http://api.weatherapi.com/v1/current.json"
+
+
 def get_weather() -> None:
-    # write your code here
-    pass
+
+    params = {"key": API_KEY, "q": REGION, "aqi": "no"}
+    response = requests.get(URL, params=params)
+    response.raise_for_status()
+
+    result = response.json()
+
+    for key, value in result.get("location", {}).items():
+        print(f"{key}: {value}")
+
+    for key in ("temp_c", "wind_kph", "wind_dir", "humidity", "last_updated"):
+        print(f"{key}: ", result.get("current", {}).get(key))
 
 
 if __name__ == "__main__":
