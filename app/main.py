2010092d26215235239dd119c692320372a527a5
Mykola Setrakov <nikset1@gmail.com>
2024-04-18 17:39:07 +0300

    flake8

--- app/main.py
+++ app/main.py
@@ -4,7 +4,9 @@ import requests
 
 
 API_KEY = os.environ.get("API_KEY")
-URL = f"https://api.weatherapi.com/v1/current.json?key={API_KEY}&q=Paris&aqi=no"
+URL = (
+    f"https://api.weatherapi.com/v1/current.json?key={API_KEY}&q=Paris&aqi=no"
+)
 
 
 def get_weather() -> None:
