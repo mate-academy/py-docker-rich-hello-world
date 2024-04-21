a8690017b3cfce4bf5dfe72b6ed8a9e43dff4ab0
ilgan <2ilgan1601@gmail.com>
2024-04-21 16:26:52 +0300

    работает докер

--- app/main.py
+++ app/main.py
@@ -8,14 +8,18 @@ def get_weather() -> None:
     city = "Paris"
     url = f"http://api.weatherapi.com/v1/current.json?key={api_key}&q=" + str(city)
     response = requests.get(url)
-    data = response.json()
-    location = data["location"]
-    current = data["current"]
-    print(f"Weather in {location["name"]}, {location["country"]}:")
-    print(f"Temp: {current["temp_c"]} °C")
-    print(f"Feels like: {current["feelslike_c"]} °C")
-    print(f"Wind speed: {current["wind_kph"]} kp/h")
-    print(f"Condition: {current["condition"]["text"]}")
+    try:
+        data = response.json()
+        location = data["location"]
+        current = data["current"]
+        print(f"Weather in {location["name"]}, {location["country"]}:")
+        print(f"Temp: {current["temp_c"]} °C")
+        print(f"Feels like: {current["feelslike_c"]} °C")
+        print(f"Wind speed: {current["wind_kph"]} kp/h")
+        print(f"Condition: {current["condition"]["text"]}")
+    except Exception:
+        print("Error. Please enter API_KEY")
+
 
 if __name__ == "__main__":
     get_weather()
