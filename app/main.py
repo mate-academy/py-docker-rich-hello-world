725166ae1364906c021100c99149e584f5d7eff7
Evgeniy Vasin <evgeniy97v@gmail.com>
2024-04-23 14:03:19 +0300

    docker-weather-api

--- app/main.py
+++ app/main.py
@@ -1,6 +1,41 @@
+import os
+import requests
+from requests.exceptions import ConnectionError
+
+API_KEY = os.environ.get("API_KEY")
+BASE_URL = "https://api.weatherapi.com/v1"
+CURRENT_WEATHER_REQUEST = "/current.json"
+CITY = "Paris"
+
+
 def get_weather() -> None:
-    # write your code here
-    pass
+    params = {
+        "key": API_KEY,
+        "q": CITY,
+    }
+    current_weather_url = f"{BASE_URL}{CURRENT_WEATHER_REQUEST}"
+
+    try:
+        response = requests.get(current_weather_url, params=params)
+        response.raise_for_status()
+
+        result = response.json()
+        location = result.get("location", {})
+        current = result.get("current", {})
+
+        city = location.get("name", "")
+        country = location.get("country", "")
+        localtime = location.get("localtime", "")
+        temperature = current.get("temp_c", "")
+        condition = current.get("condition", {}).get("text", "")
+
+        print(f"Performing request to Weather API for city {city}...")
+        print(f"{city}/{country} {localtime} Weather: {temperature} Celsius, {condition}")
+
+    except ConnectionError:
+        print("Failed to connect to the Weather API.")
+    except requests.HTTPError:
+        print("Invalid API KEY or request failed.")
 
 
 if __name__ == "__main__":
