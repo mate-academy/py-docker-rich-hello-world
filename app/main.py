6926e8aad6d589f2fcda5584e061d51977100980
Anton Tkachuk <tkacuk2291@gmail.com>
2024-04-27 00:47:57 +0300

    edit flakes

--- app/main.py
+++ app/main.py
@@ -1,18 +1,15 @@
 import requests
 import os
+
 # from dotenv import load_dotenv
 # load_dotenv('config.env')
 
-api_key = os.getenv('WEATHER_API_KEY')
+api_key = os.getenv("WEATHER_API_KEY")
 
 
 def get_weather() -> None:
     urls = "http://api.weatherapi.com/v1/current.json"
-    params = {
-        'q': 'Paris',
-        'lang': 'uk',
-        'key': f'{api_key}'
-    }
+    params = {"q": "Paris", "lang": "uk", "key": f"{api_key}"}
     response = requests.get(urls, params=params)
     if response.status_code == 200:
         data = response.json()
@@ -20,7 +17,7 @@ def get_weather() -> None:
     # elif not api_key:
 
     else:
-        print('Error:', response.status_code)
+        print("Error:", response.status_code)
 
 
 if __name__ == "__main__":
