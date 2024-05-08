6133dfea8c4866b73d18de4bc17751d4682d3050
RomanHlodann <roman.hlodann@gmail.com>
2024-05-08 20:43:34 +0300

    develop Docker Weather API

--- app/main.py
+++ app/main.py
@@ -1,6 +1,21 @@
+import requests
+import os
+from dotenv import load_dotenv
+
+
+script_dir = os.path.dirname(os.path.abspath(__file__))
+parent_dir = os.path.dirname(script_dir)
+
+load_dotenv(os.path.join(parent_dir, ".env"))
+
+URL = "https://api.weatherapi.com/v1/forecast.json"
+CITY = "Paris"
+
+
 def get_weather() -> None:
-    # write your code here
-    pass
+    key = os.getenv("API_KEY", "")
+    response = requests.get(f"{URL}?q={CITY}&key={key}")
+    print(response.text)
 
 
 if __name__ == "__main__":
