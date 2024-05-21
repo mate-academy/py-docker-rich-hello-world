6eaedb8ee9e01507f63e63169899c58a98e7ae94
Oleksandra <aleksandra@regdesk.co>
2024-05-21 22:19:02 +0300

    Solution

---
+++ Dockerfile
@@ -0,0 +1,13 @@
+FROM python:3.12-alpine
+LABEL authors="aleksandra.prognimak@gmail.com"
+
+ENV PYTHONUNBUFFERED=1
+
+WORKDIR app/
+
+COPY requirements.txt requirements.txt
+RUN pip install -r requirements.txt
+
+COPY . .
+
+CMD ["python", "-m", "app.main"]
--- app/main.py
+++ app/main.py
@@ -1,6 +1,32 @@
+import os
+import requests
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+API_KEY = os.environ.get("API_KEY")
+BASE_URL = "http://api.weatherapi.com/v1/current.json"
+CITY = "Paris"
+
+
 def get_weather() -> None:
-    # write your code here
-    pass
+    request = requests.get(BASE_URL, params={"key": API_KEY, "q": CITY})
+
+    if request.status_code == 200:
+        print(f"Performing request to Weather API for city {CITY}...")
+
+        data = request.json()
+        location = data.get("location")
+        country = location.get("country")
+        time = location.get("localtime")
+        current = data.get("current")
+        temp = current.get("temp_c")
+        condition = current.get("condition").get("text")
+
+        print(f"{CITY}/{country} {time} Weather: {temp} Celsius, {condition}")
+    else:
+        print("Something went wrong...")
 
 
 if __name__ == "__main__":
