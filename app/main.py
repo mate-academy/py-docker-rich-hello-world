19bc2ce539215753b458617e4cb7787cd08b98b8
Andrii Zubchenko <152086790+dissom@users.noreply.github.com>
2024-05-25 15:59:13 +0300

    Docker weather API

--- app/main.py
+++ app/main.py
@@ -1,6 +1,26 @@
+import requests
+import os
+
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
+
+API_KEY = os.environ.get("API_KEY")
+URL = "http://api.weatherapi.com/v1/current.json?"
+FILTERING = "Kyiv"
+
+
 def get_weather() -> None:
-    # write your code here
-    pass
+    response = requests.get(URL, params={"key": API_KEY, "q": FILTERING})
+    if response.status_code == 200:
+        data = response.json()
+        location = f"{data['location']['name']}/{data['location']['country']}"
+        localtime = data["location"]["localtime"]
+        temperature = data["current"]["temp_c"]
+        weather = data["current"]["condition"]["text"]
+        print(f"{location} {localtime} Weather: {temperature} Celsiush {weather}")
 
 
 if __name__ == "__main__":
