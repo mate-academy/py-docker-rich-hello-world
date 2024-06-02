0832fc3accb44139e7060c3c2534aa38dff3edc8
Mykhailo Yemets <mikhailyemets.pl@gmail.com>
2024-06-02 16:47:54 +0200

    docker

--- app/main.py
+++ app/main.py
@@ -1,6 +1,26 @@
+import os
+import requests
+
+API_KEY = os.environ.get("API_KEY")
+FILTERING = "Paris"
+URL = "https://api.weatherapi.com/v1"
+
+
 def get_weather() -> None:
-    # write your code here
-    pass
+    if not API_KEY:
+        raise ValueError(
+            "No API key provided.Set the WEATHER_API_KEY environment variable."
+        )
+
+    url = f"{URL}/current.json?key={API_KEY}&q={FILTERING}"
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        data = response.json()
+        print("Current temperature in", FILTERING, ":",
+              data["current"]["temp_c"], "°C")
+    else:
+        print(f"Error: {response.status_code}")
 
 
 if __name__ == "__main__":
