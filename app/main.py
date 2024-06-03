4e5474cb8b77f4f07ad231c11a274b570bf84b63
Mykhailo Yemets <mikhailyemets.pl@gmail.com>
2024-06-03 20:49:41 +0200

    refactor using params

--- app/main.py
+++ app/main.py
@@ -2,24 +2,24 @@ import os
 import requests
 
 API_KEY = os.environ.get("API_KEY")
-FILTERING = "Paris"
-URL = f"https://api.weatherapi.com/v1/current.json?key={API_KEY}&q={FILTERING}"
+BASE_URL = "https://api.weatherapi.com/v1/current.json?"
 
-def get_weather() -> None:
+
+def get_weather(city: str) -> None:
     if not API_KEY:
         raise ValueError(
             "No API key provided.Set the WEATHER_API_KEY environment variable."
         )
 
-    response = requests.get(URL)
+    response = requests.get(BASE_URL, params={"key": API_KEY, "q": city})
 
     if response.status_code == 200:
         data = response.json()
-        print("Current temperature in", FILTERING, ":",
+        print("Current temperature in", city, ":",
               data["current"]["temp_c"], "°C")
     else:
         print(f"Error: {response.status_code}")
 
 
 if __name__ == "__main__":
-    get_weather()
+    get_weather(city="Paris")
