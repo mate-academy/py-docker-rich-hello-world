2399e5a4fb53124b0c836ef32075efd17d839eaf
Alexander Kisliy <108825919+SashaKisliy@users.noreply.github.com>
2024-06-05 02:45:31 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,6 +1,20 @@
+import os
+import requests
+
+
 def get_weather() -> None:
-    # write your code here
-    pass
+    api_key = os.getenv("API_KEY")
+    if not api_key:
+        raise ValueError("No API key provided")
+
+    url = f"https://api.weatherapi.com/v1/current.json?key={api_key}&q=Paris"
+    response = requests.get(url)
+    if response.status_code == 200:
+        weather_data = response.json()
+        print(f"Current weather in Paris: "
+              f" {weather_data["current"]["temp_c"]}°C")
+    else:
+        print(f"Failed to get weather data: {response.status_code}")
 
 
 if __name__ == "__main__":
