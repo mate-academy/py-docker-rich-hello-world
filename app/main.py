42f8f06986c1952cbe86bbc8b7dc34296563a94e
Vladyslav Supruniuk <Vladyslav.Supruniuk@contracted.pmi.com>
2024-06-04 18:38:36 +0300

    Fix

--- app/main.py
+++ app/main.py
@@ -4,18 +4,22 @@ import requests
 
 
 def get_weather() -> None:
-    print("Performing request to Weather API for city Paris...")
+    try:
+        print("Performing request to Weather API for city Paris...")
 
-    api_key = os.environ["API_KEY"]
-    date_now = datetime.now().strftime("%Y-%m-%d %H:%M")
-    response = requests.get(
-        f"https://api.weatherapi.com/v1/current.json"
-        f"?key={api_key}&q=Paris&aqi=no"
-    ).json()
-    temp = response["current"]["temp_c"]
-    condition = response["current"]["condition"]["text"]
+        api_key = os.environ["API_KEY"]
+        date_now = datetime.now().strftime("%Y-%m-%d %H:%M")
+        response = requests.get(
+            f"https://api.weatherapi.com/v1/current.json"
+            f"?key={api_key}&q=Paris&aqi=no"
+        ).json()
+        temp = response["current"]["temp_c"]
+        condition = response["current"]["condition"]["text"]
 
-    print(f"Paris/France {date_now} Weather: {temp} Celsius, {condition}")
+        print(f"Paris/France {date_now} Weather: {temp} Celsius, {condition}")
+
+    except Exception as e:
+        print("Error: ", e)
 
 
 if __name__ == "__main__":
