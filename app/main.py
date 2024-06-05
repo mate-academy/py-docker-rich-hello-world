cf3843420647092fc582d7de2768e609651e554a
Vitalii <vitalikbashkiser@gmail.com>
2024-06-05 22:54:29 +0300

    Solution#2

--- app/main.py
+++ app/main.py
@@ -1,7 +1,22 @@
-def get_weather() -> None:
-    # write your code here
-    pass
+import os
+import requests
+
+
+def get_weather(api_key: str):
+    url = f"http://api.weatherapi.com/v1/current.json?key={api_key}&q=Paris"
+    response = requests.get(url)
+    if response.status_code == 200:
+        data = response.json()
+        location = data["location"]["name"]
+        temp_c = data["current"]["temp_c"]
+        condition = data["current"]["condition"]["text"]
+        return f"Weather report for {location}: {temp_c}°C, {condition}"
+    else:
+        return "Failed to get weather data"
 
 
 if __name__ == "__main__":
-    get_weather()
+    api_key = os.getenv("API_KEY")
+    if not api_key:
+        raise ValueError("API_KEY environment variable is not set")
+    print(get_weather(api_key))
