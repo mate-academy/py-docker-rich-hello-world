7ddddb8db40a7290ebee483ab13192fed88aef96
Yevheniia Kondrashova <evgeniia.kondrashova@gmail.com>
2024-06-05 11:25:30 +0300

    Add get_weather function to main.py

--- app/main.py
+++ app/main.py
@@ -1,6 +1,24 @@
+import os
+
+import requests
+from dotenv import load_dotenv
+
+load_dotenv()
+
+API_KEY = "e76c81a005944e4bac6142817240406"
+KEY = os.getenv("API_KEY")
+CITY = "Paris"
+
+URL = f"http://api.weatherapi.com/v1/current.json?"
+
+
 def get_weather() -> None:
-    # write your code here
-    pass
+    weather = requests.get(URL, params={"key": KEY, "q": CITY}).json()
+    print(
+        f"{weather['location']['name']}({weather['location']['country']})\n"
+        f"Time: {weather['location']['localtime']}\n"
+        f'Weather: {weather["current"]["temp_c"]} Celsius'
+    )
 
 
 if __name__ == "__main__":
