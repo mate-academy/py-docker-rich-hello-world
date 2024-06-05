26e6a2e904b29ced6e9cf983808b8ebb588b485a
Yevheniia Kondrashova <evgeniia.kondrashova@gmail.com>
2024-06-05 22:56:33 +0300

    Refactor main.py
    Fix flake8 error:
    F541 f-string is missing placeholders

--- app/main.py
+++ app/main.py
@@ -9,15 +9,15 @@ API_KEY = "e76c81a005944e4bac6142817240406"
 KEY = os.getenv("API_KEY")
 CITY = "Paris"
 
-URL = f"http://api.weatherapi.com/v1/current.json?"
+URL = "http://api.weatherapi.com/v1/current.json?"
 
 
 def get_weather() -> None:
     weather = requests.get(URL, params={"key": KEY, "q": CITY}).json()
     print(
-        f"{weather['location']['name']}({weather['location']['country']})\n"
-        f"Time: {weather['location']['localtime']}\n"
-        f'Weather: {weather["current"]["temp_c"]} Celsius'
+        f"{weather["location"]["name"]}({weather["location"]["country"]})\n"
+        f"Time: {weather["location"]["localtime"]}\n"
+        f"Weather: {weather["current"]["temp_c"]} Celsius"
     )
 
 
