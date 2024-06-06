eb8c4f41e69be2ed89ab02c35077cfcd0c431fec
semermaks <semermaks@gmail.com>
2024-06-06 12:07:34 +0300

    Flake8 fix

--- app/main.py
+++ app/main.py
@@ -3,7 +3,7 @@ from datetime import datetime
 
 API_KEY = "2a40a2a2837b4d50ae954326240506"
 LOCATION = "Kharkiv"
-URL = f"http://api.weatherapi.com/v1/current.json"
+URL = "http://api.weatherapi.com/v1/current.json"
 
 
 def get_weather() -> None:
@@ -11,12 +11,15 @@ def get_weather() -> None:
         response = requests.get(URL, params={"key": API_KEY, "q": LOCATION})
         data = response.json()
         if response.status_code == 200:
-            location_name = data['location']['name']
-            country = data['location']['country']
-            temp_c = data['current']['temp_c']
-            condition = data['current']['condition']['text']
+            location_name = data["location"]["name"]
+            country = data["location"]["country"]
+            temp_c = data["current"]["temp_c"]
+            condition = data["current"]["condition"]["text"]
             current_time = datetime.now().strftime("%y-%m-%d %H:%M")
-            print(f"{location_name}/{country} {current_time} Weather: {temp_c} Celsius, {condition}")
+            print(
+                f"{location_name}/{country} {current_time}"
+                f" Weather: {temp_c} Celsius, {condition}"
+            )
         else:
             print(f"Failed to fetch weather data: {data['error']['message']}")
     except Exception as e:
