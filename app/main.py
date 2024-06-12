f6709e16e44463010253ee87c10a2e40be841cc1
dkotkod <dkotkod@gmail.com>
2024-06-12 15:14:21 +0300

    Solution_new

--- app/main.py
+++ app/main.py
@@ -14,10 +14,11 @@ def get_weather() -> None:
     print(f"Performing request to Weather API for city {FILTERING}...")
     result = requests.get(URL, params={"key": API_KEY, "q": FILTERING}).json()
     print(
-        f"{result["location"]["name"]}/{result["location"]["country"]} "
-        f"{result["location"]["localtime"]} "
-        f"Weather: {result["current"]["temp_c"]} Celsius, "
-        f"{result["current"]["condition"]["text"]}")
+        f'{result["location"]["name"]}/{result["location"]["country"]} '
+        f'{result["location"]["localtime"]} '
+        f'Weather: {result["current"]["temp_c"]} Celsius, '
+        f'{result["current"]["condition"]["text"]}'
+    )
 
 
 if __name__ == "__main__":
