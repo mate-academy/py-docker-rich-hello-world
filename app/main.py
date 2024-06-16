e99b2ab147a4907832f887e4e1095def2f10ba61
dmytro-yefimenko <dmytro@yefimenko.uk>
2024-06-16 10:04:54 +0300

    Solution: docker weather api

--- app/main.py
+++ app/main.py
@@ -1,6 +1,31 @@
+import os
+import requests
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+CITY = "Paris"
+API_KEY = os.getenv("API_KEY")
+URL = "https://api.weatherapi.com/v1/current.json"
+
+
 def get_weather() -> None:
-    # write your code here
-    pass
+    params = {"key": API_KEY, "q": CITY}
+
+    data = requests.get(URL, params).json()
+
+    country = data["location"]["country"]
+    last_updated = data["current"]["last_updated"]
+    temp_celsius = data["current"]["temp_c"]
+    weather_text = data["current"]["condition"]["text"]
+
+    print(
+        f"Weather API request for {CITY}/{country}\n"
+        f"Date/Time: {last_updated}\n"
+        f"Weather: {temp_celsius} Celsius, {weather_text}"
+    )
 
 
 if __name__ == "__main__":
