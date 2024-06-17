598fe79648a95532f1e70b9016b2836e81744c4d
Vlad_work <mr.darmstadtium@gmail.com>
2024-06-17 16:55:16 +0200

    can't use api_key form .env

--- app/main.py
+++ app/main.py
@@ -1,16 +1,28 @@
-import json
-import urllib.request
-requests
+import requests
+import os
+
+
+WEATHER_API_URL = "http://api.weatherapi.com/v1/current.json"
+API_KEY = os.environ.get("API_KEY")
 
 
 def get_weather(api_key: str) -> None:
+    with requests.get(
+            WEATHER_API_URL,
+            params={"key": api_key, "q": "London"}
+    ) as request:
+        if request.status_code != 200:
+            print("Error: Can't get data from Weather API.")
+            return
 
-    url = f"http://api.weatherapi.com/v1/current.json?key={api_key}&q=London"
-
-    with urllib.request.urlopen(url) as response:
-        data = json.load(response)
-        print(data)
+        request = request.json()
+        print(
+            f"{request['location']['name']}/{request['location']['country']} "
+            f"{request['current']['last_updated']} "
+            f"Weather: {request['current']['temp_c']} Celsius, "
+            f"{request['current']['condition']['text']}"
+        )
 
 
 if __name__ == "__main__":
-    get_weather("ae04ee4ff49349f0ba8180114241306")
+    get_weather(API_KEY)
