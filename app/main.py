f81a50845dded1ce5206da71190c0fed40f008e1
Alex Grytsai <a.grytsai1987@gmail.com>
2024-06-30 14:08:44 +0300

    fixed all notices

--- app/main.py
+++ app/main.py
@@ -7,13 +7,20 @@ def get_weather(city: str = "Paris") -> None:
     print(f"Performing request to Weather API for city {city}...")
     url = (f"https://api.weatherapi.com/v1/current.json"
            f"?key={os.getenv('API_KEY')}&q={city}")
-    response = (requests.get(url)).json()
-    print(
-        f"{response['location']['name']}/{response['location']['country']} "
-        f"{response['location']['localtime']} "
-        f"Weather: {response['current']['temp_c']} Celsius, "
-        f"{response['current']['condition']['text']}"
-    )
+    response = requests.get(url)
+    if response.status_code != 200:
+        print(f"Error: {response.status_code}")
+    else:
+        response = response.json()
+        city = response["location"]["name"]
+        country = response["location"]["country"]
+        local_time = response["location"]["localtime"]
+        temperature = response["current"]["temp_c"]
+        condition = response["current"]["condition"]["text"]
+        print(
+            f"{city}/{country} {local_time} Weather: {temperature} Celsius, "
+            f"{condition}"
+        )
 
 
 if __name__ == "__main__":
