53dee3eff732b3f2dcb34b4ed7e18fd142ca59eb
Alex Grytsai <a.grytsai1987@gmail.com>
2024-06-30 07:39:12 +0300

    fixed flake8

--- app/main.py
+++ app/main.py
@@ -5,13 +5,14 @@ import requests
 
 def get_weather(city: str = "Paris") -> None:
     print(f"Performing request to Weather API for city {city}...")
-    url = (f"https://api.weatherapi.com/v1/current.json"
-           f"?key={os.getenv("API_KEY")}&q={city}")
+    url = f"https://api.weatherapi.com/v1/current.json?key={os.getenv("API_KEY")}&q={city}"
     response = (requests.get(url)).json()
-    print(f"{response["location"]["name"]}/{response["location"]["country"]} "
-          f"{response["location"]["localtime"]} "
-          f"Weather: {response["current"]["temp_c"]} Celsius, "
-          f"{response["current"]["condition"]["text"]}")
+    print(
+        f"{response["location"]["name"]}/{response["location"]["country"]} "
+        f"{response["location"]["localtime"]} "
+        f"Weather: {response["current"]["temp_c"]} Celsius, "
+        f"{response["current"]["condition"]["text"]}"
+    )
 
 
 if __name__ == "__main__":
