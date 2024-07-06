e7d56b50f28b5a42c6d9f6cda90e372398097f9f
Vadim Babii <judviiioff@gmail.com>
2024-07-06 21:02:03 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,6 +1,22 @@
+import os
+
+import requests
+
+API_KEY = os.getenv("API_KEY")
+URL = "https://api.weatherapi.com/v1/current.json?"
+FILTERING = "Paris"
+
+if not API_KEY:
+    raise ValueError("API_KEY must be set")
+
+
 def get_weather() -> None:
-    # write your code here
-    pass
+    result = requests.get(URL, params={"key": API_KEY, "q": FILTERING}).json()
+    print(
+        f"In {result['location']['country']} - {result['location']['name']}, "
+        f"current weather temperature is {result['current']['temp_c']} celsius"
+        f" and current condition is {result['current']['condition']['text']}"
+    )
 
 
 if __name__ == "__main__":
