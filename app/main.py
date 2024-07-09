a24b1bc8e49ea173b87bf9cd0d2c30fd5fbb2c6e
skeletronys <mykytcey@gmail.com>
2024-07-09 17:38:16 +0200

    fix

--- app/main.py
+++ app/main.py
@@ -2,30 +2,27 @@ import os
 import requests
 
 CITY = "Paris"
-
+API_KEY = os.getenv("API_KEY")
 
 def get_weather() -> None:
-    api_key = os.getenv("API_KEY")
-
-    if not api_key:
-        print("API_KEY environment variable not set.")
-        return
+    if not API_KEY:
+        raise EnvironmentError("API_KEY environment variable not set.")
 
     url = "http://api.openweathermap.org/data/2.5/weather"
     params = {
         "q": CITY,
-        "appid": api_key,
+        "appid": API_KEY,
         "units": "metric"
     }
-    response = requests.get(url)
-    data = requests.get(url, params=params)
+    response = requests.get(url, params=params)
 
     if response.status_code == 200:
+        data = response.json()
         temp = data["main"]["temp"]
         weather = data["weather"][0]["description"]
         print(f"Weather in {CITY}: {weather.capitalize()}, {temp}°C")
     else:
-        print(f"Error: {data['message']}")
+        print(f"Error: {response.json()['message']}")
 
 
 if __name__ == "__main__":
