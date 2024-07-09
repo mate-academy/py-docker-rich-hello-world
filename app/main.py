cb68908827acbaddd68340e312ff9c5d81d41d27
skeletronys <mykytcey@gmail.com>
2024-07-09 02:12:33 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,6 +1,25 @@
+import os
+import requests
+
+
 def get_weather() -> None:
-    # write your code here
-    pass
+    api_key = os.getenv("API_KEY")
+    city = "Paris"
+
+    if not api_key:
+        print("API_KEY environment variable not set.")
+        return
+
+    url = f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}&units=metric"
+    response = requests.get(url)
+    data = response.json()
+
+    if response.status_code == 200:
+        temp = data["main"]["temp"]
+        weather = data["weather"][0]["description"]
+        print(f"Weather in {city}: {weather.capitalize()}, {temp}°C")
+    else:
+        print(f"Error: {data["message"]}")
 
 
 if __name__ == "__main__":
