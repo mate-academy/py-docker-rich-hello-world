a97dea7ca8213b5b12492edad511e34d5164d36c
Alla Kuksa <alla.kuksa92@gmail.com>
2024-07-10 14:57:50 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,6 +1,33 @@
+from __future__ import print_function
+import os
+import swagger_client
+import requests
+from swagger_client.rest import ApiException
+from dotenv import load_dotenv
+
+
+load_dotenv()
+API_KEY = os.getenv("API_KEY")
+CITY = "Paris"
+LANG = "en"
+URL = "https://api.weatherapi.com/v1/current.json?"
+
+
 def get_weather() -> None:
-    # write your code here
-    pass
+    configuration = swagger_client.Configuration()
+    configuration.api_key["key"] = API_KEY
+
+    url = f"{URL}key={API_KEY}&q={CITY}&lang={LANG}"
+
+    try:
+        response = requests.get(url).json()
+        print(f"{response['location']['country']}/"
+              f"{response['location']['name']} "
+              f"{response['location']['localtime']} "
+              f"Weather: {response['current']['temp_c']} Celsius, "
+              f"{response['current']['condition']['text']}")
+    except ApiException as e:
+        print(f"Exception when calling Weather API: {e}")
 
 
 if __name__ == "__main__":
