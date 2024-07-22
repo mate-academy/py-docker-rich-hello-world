56023539354f24b3a7a98a785fa3f734e30a87e4
Roman Shupak <romanshupak808@gmail.com>
2024-07-22 23:14:34 +0300

    "add the rest"

--- app/main.py
+++ app/main.py
@@ -1,6 +1,35 @@
+import os
+
+import requests
+
+URL = "https://api.weatherapi.com/v1/current.json"
+CITY = "Paris"
+API_KEY = os.environ.get("API_KEY")
+
+
 def get_weather() -> None:
-    # write your code here
-    pass
+    url = f"{URL}?key={API_KEY}&q={CITY}"
+    response = requests.get(url).json()
+
+    city = response["location"]["name"]
+    country = response["location"]["country"]
+    current_time = response["location"]["localtime"]
+    temp_celsius = response["current"]["temp_c"]
+    current_weather = response["current"]["condition"]["text"]
+
+    weather_condition_1st_line = (
+        f"Performing request to weather API for city {city}...")
+    weather_condition_2nd_line = (
+        f"{city}/{country} {current_time} "
+        f"Weather: {temp_celsius} "
+        f"Celsius, {current_weather}"
+    )
+
+    if "error" in response:
+        print("Error:", response["error"]["message"])
+    else:
+        print(weather_condition_1st_line)
+        print(weather_condition_2nd_line)
 
 
 if __name__ == "__main__":
