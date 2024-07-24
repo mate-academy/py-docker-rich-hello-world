125a5b1c492e57474cfc772f4b188b00e6b949d3
Roman Shupak <romanshupak808@gmail.com>
2024-07-24 12:35:07 +0300

    add check for response status 200 and after get data

--- app/main.py
+++ app/main.py
@@ -9,28 +9,33 @@ API_KEY = os.environ.get("API_KEY")
 
 def get_weather() -> None:
     url = f"{URL}?key={API_KEY}&q={CITY}"
-    response = requests.get(url).json()
-    print(response)
+    response = requests.get(url)
 
-    city = response["location"]["name"]
-    country = response["location"]["country"]
-    current_time = response["location"]["localtime"]
-    temp_celsius = response["current"]["temp_c"]
-    current_weather = response["current"]["condition"]["text"]
+    if response.status_code == 200:
+        data = response.json()
 
-    weather_condition_1st_line = (
-        f"Performing request to weather API for city {city}...")
-    weather_condition_2nd_line = (
-        f"{city}/{country} {current_time} "
-        f"Weather: {temp_celsius} "
-        f"Celsius, {current_weather}"
-    )
+        city = data["location"]["name"]
+        country = data["location"]["country"]
+        current_time = data["location"]["localtime"]
+        temp_celsius = data["current"]["temp_c"]
+        current_weather = data["current"]["condition"]["text"]
 
-    if "error" in response:
-        print("Error:", response["error"]["message"])
-    else:
+        weather_condition_1st_line = (
+            f"Performing request to weather API for city {city}...")
+        weather_condition_2nd_line = (
+             f"{city}/{country} {current_time} "
+             f"Weather: {temp_celsius} "
+             f"Celsius, {current_weather}"
+        )
+
         print(weather_condition_1st_line)
         print(weather_condition_2nd_line)
+    else:
+        error_message = (
+            response.json().get("error", {})
+            .get("message", "Unknown error occurred")
+        )
+        print("Error:", error_message)
 
 
 if __name__ == "__main__":
