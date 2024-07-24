dfe94e3ab95095322d4cae93f1f057a74ae343a1
Roma Nesterenko <rom.puh@gmail.com>
2024-07-24 21:38:05 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,6 +1,24 @@
+import os
+import requests
+
+
 def get_weather() -> None:
-    # write your code here
-    pass
+    API_KEY = os.environ.get("API_KEY")
+    CITY = "Paris"
+    URL = "http://api.weatherapi.com/v1/current.json?"
+
+    res = requests.get(URL + f"key={API_KEY}&q={CITY}")
+
+    data = res.json()
+
+    city = data["location"]["name"]
+    country = data["location"]["country"]
+    date = data["location"]["localtime"]
+    temperature = data["current"]["temp_c"]
+    sky = data["current"]["condition"]["text"]
+
+    print(f"Performing request to Weather API for city {city}...")
+    print(f"{city}/{country} {date} Weather: {temperature}, {sky}")
 
 
 if __name__ == "__main__":
