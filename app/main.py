68630550de45165caaf8a8a4ce6dbf825cd5596f
Vlad <vlad@example.com>
2024-07-31 13:50:00 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -20,12 +20,14 @@ def get_weather() -> None:
     res = requests.get(url_with_params)
     if res.status_code == 200:
         data = res.json()
-        location = data["location"]
+        name, country = data["location"]["mame"], data["location"]["country"]
         current_data = data["current"]
+        last_updated = current_data["last_updated"]
         temp = current_data["temp_c"]
-        print(f"{location["name"]}/{location["country"]} ")
-        print(f"{current_data["last_updated"]} Weather: {temp} Celsius, ")
-        print(f"{current_data["condition"]["text"]}")
+        text = current_data["condition"]["text"]
+        print(f"{name}/{country} ")
+        print(f"{last_updated} Weather: {temp} Celsius, ")
+        print(f"{text}")
 
 
 if __name__ == "__main__":
