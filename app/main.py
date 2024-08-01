bd04e3cb6e8f6b0e20c5a811b256617d5ce168ff
timur <stukantimur811@gmail.com>
2024-08-01 15:34:16 +0300

    created docker file for weather api for paris

--- app/main.py
+++ app/main.py
@@ -1,6 +1,25 @@
+import time
+
+import requests
+import json
+import os
+
+
 def get_weather() -> None:
-    # write your code here
-    pass
+    API_KEY = os.getenv('API_KEY')
+    if not API_KEY:
+        raise ValueError("You need to set API_KEY environment variable")
+    res = requests.get(
+        f"http://api.weatherapi.com/v1/current.json?"
+        f"key={API_KEY}&q=Paris&aqi=no"
+    )
+    content = json.loads(res.content)
+    print("Performing request to weather api for city Paris....")
+    time.sleep(2)
+    print(f"{content['location']['country']}/{content['location']['name']} "
+          f"{content['location']['localtime']} "
+          f"Weather: {content['current']['temp_c']} "
+          f"Celsius, {content['current']['condition']['text']}")
 
 
 if __name__ == "__main__":
