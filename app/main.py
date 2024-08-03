2bdc65ee33844263616dc82b968322fb1f75fc1e
Bohdan Zinchenko <bogdan.zinchenko.2019@gmail.com>
2024-08-04 00:00:58 +0300

    py-docker-weather-api v2

--- app/main.py
+++ app/main.py
@@ -8,13 +8,14 @@ CITY = "Kyiv"
 BASE_URL = "http://api.weatherapi.com/v1/current.json"
 
 
-def get_weather():
+def get_weather() -> None:
     response = requests.get(f"{BASE_URL}?key={KEY}&q={CITY}",)
     data = response.json()
     print(f"Performing request to Weather API for city {CITY}...")
     print(f"{data['location']['name']}/{data['location']['country']}"
           f" {data['location']['localtime']} "
-          f"Weather: {data['current']['temp_c']} Celsius, {data['current']['condition']['text']}")
+          f"Weather: {data['current']['temp_c']} "
+          f"Celsius, {data['current']['condition']['text']}")
 
 
 if __name__ == "__main__":
