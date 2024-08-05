d8e01933df4ba594f2665f3dc2243210d0e15016
Andrii1000 <127493083+Andrii1000@users.noreply.github.com>
2024-08-05 15:34:35 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,6 +1,18 @@
+import os
+import requests
+
+
+API_KEY = os.environ["API_KEY"]
+BASE_URL = "http://api.weatherapi.com/v1/current.json"
+CITY = "Lviv"
+
+
 def get_weather() -> None:
     # write your code here
-    pass
+    response = requests.get(
+        f"{BASE_URL}?key={API_KEY}&q={CITY}"
+    )
+    print(response.json())
 
 
 if __name__ == "__main__":
