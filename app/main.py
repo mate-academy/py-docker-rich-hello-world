9ccee7f060f973b43da2fe18fac1480336699946
SuskyiVolodymyr <suskyivolodymyr@gmail.com>
2024-08-05 18:07:00 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,6 +1,30 @@
+import os
+import requests
+
+
+URL = "https://api.weatherapi.com/v1/current.json"
+CITY = "Paris"
+API_KEY = os.environ.get("API_KEY")
+
+
 def get_weather() -> None:
-    # write your code here
-    pass
+    url = f"{URL}?key={API_KEY}&q={CITY}"
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        data = response.json()
+        city = data["location"]["name"]
+        country = data["location"]["country"]
+        current_time = data["location"]["localtime"]
+        temp = data["current"]["temp_c"]
+        weather = data["current"]["condition"]["text"]
+        print(f"{city}/{country} {current_time} Weather: {temp} Celsius, {weather}")
+    else:
+        error_message = (
+            response.json().get("error", {})
+            .get("message", "Unknown error occurred")
+        )
+        print("Error:", error_message)
 
 
 if __name__ == "__main__":
