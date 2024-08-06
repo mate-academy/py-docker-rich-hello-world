0b2e0777cfc4055f90e1f456cbb1a245d9bd5819
SuskyiVolodymyr <suskyivolodymyr@gmail.com>
2024-08-06 15:24:55 +0300

    add try block

--- app/main.py
+++ app/main.py
@@ -9,23 +9,26 @@ API_KEY = os.environ.get("API_KEY")
 
 def get_weather() -> None:
     url = f"{URL}?key={API_KEY}&q={CITY}"
-    response = requests.get(url)
+    try:
+        response = requests.get(url)
+        if response.status_code == 200:
+            data = response.json()
+            city = data["location"]["name"]
+            country = data["location"]["country"]
+            current_time = data["location"]["localtime"]
+            temp = data["current"]["temp_c"]
+            weather = data["current"]["condition"]["text"]
+            print(f"{city}/{country} {current_time} "
+                  f"Weather: {temp} Celsius, {weather}")
+        else:
+            error_message = (
+                response.json().get("error", {})
+                .get("message", "Unknown error occurred")
+            )
+            print("Error:", error_message)
 
-    if response.status_code == 200:
-        data = response.json()
-        city = data["location"]["name"]
-        country = data["location"]["country"]
-        current_time = data["location"]["localtime"]
-        temp = data["current"]["temp_c"]
-        weather = data["current"]["condition"]["text"]
-        print(f"{city}/{country} {current_time} "
-              f"Weather: {temp} Celsius, {weather}")
-    else:
-        error_message = (
-            response.json().get("error", {})
-            .get("message", "Unknown error occurred")
-        )
-        print("Error:", error_message)
+    except Exception as e:
+        print(f"Error: {e}, please ensure that link is valid")
 
 
 if __name__ == "__main__":
