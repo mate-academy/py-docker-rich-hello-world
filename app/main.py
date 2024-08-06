44bd6c72e8213c0e140b54e2b97db6df867557d2
Serhii <Musienko.S.A.1983@gmail.com>
2024-08-06 19:11:48 +0300

    add try

--- app/main.py
+++ app/main.py
@@ -11,25 +11,32 @@ CITY = "Paris"
 
 
 def get_weather() -> None:
-    print(f"Start request to Weather API for city {CITY}...")
-    res = requests.get(URL, params={"key": API_KEY, "q": CITY})
+    try:
+        print(f"Start request to Weather API for city {CITY}...")
+        res = requests.get(URL, params={"key": API_KEY, "q": CITY})
+        res.raise_for_status()
 
-    if res.status_code == 200:
-        data = res.json()
-        location_name = data["location"]["name"]
-        country = data["location"]["country"]
-        local_time = data["location"]["localtime"]
-        temp_c = data["current"]["temp_c"]
-        weather = data["current"]["condition"]["text"]
+        try:
+            data = res.json()
+            location_name = data["location"]["name"]
+            country = data["location"]["country"]
+            local_time = data["location"]["localtime"]
+            temp_c = data["current"]["temp_c"]
+            weather = data["current"]["condition"]["text"]
 
-        print(
-            f"{location_name}/{country} "
-            f"{local_time} Weather: {temp_c} Celsius, {weather}"
-        )
-    else:
-        print(
-            f"status code {res.status_code} something went wrong {res.reason}"
-        )
+            print(
+                f"{location_name}/{country} "
+                f"{local_time} Weather: {temp_c} Celsius, {weather}"
+            )
+        except ValueError:
+            print("Failed to parse JSON response.")
+
+    except requests.RequestException as e:
+        print(f"Request failed: {e}")
+    except KeyError as e:
+        print(f"Missing key in response data: {e}")
+    except Exception as e:
+        print(f"An unexpected error occurred: {e}")
 
 
 if __name__ == "__main__":
