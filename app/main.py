6d17cfa18a16baf79f0b01388090cac51834e8eb
Dima <dima2000yan@gmail.com>
2024-08-06 23:32:51 +0300

    solution fix

--- app/main.py
+++ app/main.py
@@ -7,21 +7,26 @@ URL = "http://api.weatherapi.com/v1/current.json"
 
 
 def get_weather(city: str) -> None:
-    res = requests.get(URL, params={"key": API_KEY, "q": city})
+    try:
+        res = requests.get(URL, params={"key": API_KEY, "q": city})
 
-    data = res.json()
-    city = data["location"]["name"]
-    country = data["location"]["country"]
-    time = data["location"]["localtime"]
-    temperature = data["current"]["temp_c"]
-    weather = data["current"]["condition"]["text"]
+        data = res.json()
+        city = data["location"]["name"]
+        country = data["location"]["country"]
+        time = data["location"]["localtime"]
+        temperature = data["current"]["temp_c"]
+        weather = data["current"]["condition"]["text"]
 
-    print(
-        f"{city}/{country} "
-        f"{time} "
-        f"Weather: {temperature} celsius, "
-        f"{weather}"
-    )
+        print(
+            f"{city}/{country} "
+            f"{time} "
+            f"Weather: {temperature} celsius, "
+            f"{weather}"
+        )
+    except requests.RequestException as e:
+        raise RuntimeError(f"Exception message: {e}")
+    except (KeyError, ValueError) as e:
+        raise RuntimeError(f"Exception message: {e}")
 
 
 if __name__ == "__main__":
