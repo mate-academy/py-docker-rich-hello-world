46ce43782ff2b5469a605476f92f7e388faab21c
Yevheniia Sosnovska <155542252+YESosnovska@users.noreply.github.com>
2024-08-09 17:22:57 +0300

    solution

---
+++ Dockerfile
@@ -0,0 +1,13 @@
+FROM python:3.12-alpine
+LABEL maintainer="sova0123456789@gmail.com"
+
+ENV PYTHOUNNBUFFERED 1
+
+WORKDIR app/
+
+COPY requirements.txt requirements.txt
+RUN pip install -r requirements.txt
+
+COPY . .
+
+CMD ["python", "app/main.py"]
--- app/main.py
+++ app/main.py
@@ -1,6 +1,18 @@
+import os
+import requests
+
+URL = "http://api.weatherapi.com/v1/current.json?"
+FILTERING = "Paris"
+
+
 def get_weather() -> None:
-    # write your code here
-    pass
+    api_key = os.getenv("API_KEY")
+    request = requests.get(URL + f"key={api_key}&q={FILTERING}").json()
+
+    temp = request["current"]["temp_c"]
+    cond = request["current"]["condition"]["text"]
+
+    print(f"{FILTERING}, weather: {temp} Celsius, {cond}")
 
 
 if __name__ == "__main__":
