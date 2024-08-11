48c1c539574490da904def930be941f4f1fd99e8
xborismenx <boris.tikhonov.21@gmail.com>
2024-08-11 17:30:14 +0300

    fixed

--- app/main.py
+++ app/main.py
@@ -15,10 +15,13 @@ def get_weather() -> None:
     result = requests.get(URL + "?" + FILTERING, params={"key": KEY_API})
     data = result.json()
 
-    location = f"{data["location"]["name"]} {data['location']['country']}"
-    weather = f"{data['current']['temp_c']}, {data['current']['condition']["text"]}"
+    location = (f"{data["location"]["name"]} "
+                f"{data["location"]["country"]}")
+    weather = (f"{data["current"]["temp_c"]}, "
+               f"{data["current"]["condition"]["text"]}")
     localtime = data["location"]["localtime"]
-    print(f"Weather in: {location}, \nTime: {localtime}. Temperature now: {weather}")
+    print(f"Weather in: {location}, \n"
+          f"Time: {localtime}. Temperature now: {weather}")
 
 
 if __name__ == "__main__":
