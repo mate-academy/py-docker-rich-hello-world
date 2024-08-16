903ce8be0785405977fd29641b594233c60eb346
Misha <mishelgrotski9@gmail.com>
2024-08-15 22:39:30 -0600

    Solution

--- app/main.py
+++ app/main.py
@@ -1,7 +1,22 @@
-def get_weather() -> None:
-    # write your code here
-    pass
+import os
 
+from dotenv import load_dotenv
+
+import requests
+
+load_dotenv()
+
+api_key = os.environ.get("API_KEY")
+
+def get_weather() -> None:
+    city = "Paris"
+    url = f"https://api.weatherapi.com/v1/current.json?key={api_key}&q={city}"
+    response = requests.get(url)
+    try:
+        weather = response.json()
+        print(f"Weather in {weather['current']['last_updated']} {city}: {weather['current']['temp_c']}C, {weather['current']['condition']['text']}")
+    except requests.exceptions.ConnectionError:
+        print("API key not provided.")
 
 if __name__ == "__main__":
     get_weather()
