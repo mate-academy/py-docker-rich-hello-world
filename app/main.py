a03ec4a2588dcb65421dc43aa0ab641c889019de
Misha <mishelgrotski9@gmail.com>
2024-08-17 16:22:17 -0600

    Solution

--- app/main.py
+++ app/main.py
@@ -14,6 +14,7 @@ def get_weather() -> None:
     url = f"https://api.weatherapi.com/v1/current.json?key={api_key}&q={city}"
     response = requests.get(url)
     try:
+        response.raise_for_status()
         weather = response.json()
         print(
             f"Weather in {weather['current']['last_updated']} {city}: "
