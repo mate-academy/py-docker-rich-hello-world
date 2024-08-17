7d988f49ac8a7983332a2f2598c0b8d279e95135
Vlad Krit <kritvladislav98@gmail.com>
2024-08-17 20:00:58 +0300

    fixes

--- app/main.py
+++ app/main.py
@@ -1,8 +1,9 @@
 import requests
+import os
 
 
 def get_weather() -> None:
-    api_key = "11ece046820c44a6877125738241708" #It's not safe to store the API key in code, but it's easier than in an environment variable :)
+    api_key = os.getenv("API_KEY")
     url = "http://api.weatherapi.com/v1/current.json"
     key = api_key
     default_city = "Kyiv"
@@ -14,7 +15,7 @@ def get_weather() -> None:
             "aqi": "no"
         }
         response = requests.get(url, params=params)
-    except:
+    except ValueError:
         print("ERROR: API_KEY is not correct or invalid.")
 
     try:
@@ -22,7 +23,7 @@ def get_weather() -> None:
         print(f"Current weather in {weather_data['location']['name']}:")
         print(f"Temperature: {weather_data['current']['temp_c']}°C")
         print(f"Weather: {weather_data['current']['condition']['text']}")
-    except:
+    except ValueError:
         print("ERROR: Connection failed.")
 
 
