2d917701ab9608fb0d9e99bae5543bdf52e47120
TarasFirst <firsttaras@mail.com>
2024-08-24 09:48:40 +0200

    create feature to display the current weather in Paris

--- app/main.py
+++ app/main.py
@@ -1,6 +1,25 @@
+import requests
+import os
+
+
 def get_weather() -> None:
-    # write your code here
-    pass
+
+    api_key = os.getenv("API_KEY")
+    if not api_key:
+        raise ValueError("API_KEY environment variable is not set.")
+
+    url = (f"http://api.weatherapi.com/v1/current.json?"
+           f"key={api_key}&q=Paris")
+    title_request = "Performing request to Weather API for city Paris..."
+    result = requests.get(url)
+    data_paris = result.json()
+    location = (f"{data_paris['location']['name']}/"
+                f"{data_paris['location']['country']}")
+    local_time = f"{data_paris['location']['localtime']}"
+    weather = (f" Weather: {data_paris['current']['temp_c']} "
+               f"Celsius, {data_paris['current']['condition']['text']}")
+    print(title_request)
+    print(location + local_time + weather)
 
 
 if __name__ == "__main__":
