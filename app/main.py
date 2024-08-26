17e2674ba4100eb9dcd4c60fd4d2635c66062517
Olha <olgazinevych@gmail.com>
2024-08-26 10:44:28 +0300

    add checks for errors in main

--- app/main.py
+++ app/main.py
@@ -9,10 +9,19 @@ FILTER = "Paris"
 
 
 def get_weather() -> None:
-    res = requests.get(
+    weather_response = requests.get(
         BASE_URL, params={"key": os.getenv("API_KEY"), "q": FILTER}
     )
-    print(res.content)
+
+    if weather_response.status_code == 200:
+        print(weather_response.content)
+    elif weather_response.status_code == 401:
+        print("API key has not been provided or is not valid.")
+    elif weather_response.status_code == 403:
+        print("API key is disabled or you don't have access to the resource.")
+    else:
+        print(f"There is an error. Try fixing this: "
+              f"{weather_response.content}")
 
 
 if __name__ == "__main__":
