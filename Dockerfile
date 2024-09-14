ab4b3909af654f74ce1e43ba8144ab7e20e057ee
Vadym Vyshnevskyi <wallian169@gmail.com>
2024-09-14 20:16:04 +0300

    Solution

---
+++ Dockerfile
@@ -0,0 +1,14 @@
+FROM python:3.9-alpine
+LABEL mantainner="wallian169@gmail.com"
+
+ENV PYTHONUNBUFFERED=1
+
+WORKDIR /app
+
+COPY requirements.txt requirements.txt
+
+RUN pip install -r requirements.txt
+
+COPY . .
+
+CMD ["python", "app/main.py"]
--- app/main.py
+++ app/main.py
@@ -1,6 +1,35 @@
+import requests
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+API_KEY = os.environ["API_KEY"]
+CITY = "Paris"
+URL = f"http://api.weatherapi.com/v1/current.json?key={API_KEY}&q={CITY}"
+
+
 def get_weather() -> None:
-    # write your code here
-    pass
+    print(f"Performing request to Weather API for city {CITY}...")
+    try:
+        response = requests.get(URL)
+        response.raise_for_status()
+        weather_data = response.json()
+
+        location = weather_data["location"]["tz_id"]
+        local_time = weather_data["location"]["localtime"]
+        temperature = weather_data["current"]["temp_c"]
+        condition = weather_data["current"]["condition"]["text"]
+
+        print(
+            f"{location} {local_time} "
+            f"Weather: {temperature} Celsius, {condition}"
+        )
+
+    except requests.exceptions.HTTPError as http_err:
+        print(f"HTTP error occurred: {http_err}")
+    except Exception as err:
+        print(f"Other error occurred: {err}")
 
 
 if __name__ == "__main__":
