3de3e3f37b7f439c2a6977bcd8191b79b30a9841
Olena Zaritska <zaritskaos@gmail.com>
2024-10-06 20:06:26 +0300

    implement py-docker-weather-api

--- app/main.py
+++ app/main.py
@@ -1,6 +1,26 @@
+import os
+import requests
+
+API_KEY = os.getenv("API_KEY")
+URL = "http://api.weatherapi.com/v1/current.json"
+
+
 def get_weather() -> None:
-    # write your code here
-    pass
+    params = {"key": API_KEY, "q": "Paris"}
+    response = requests.get(URL, params=params)
+    if response.status_code == 200:
+        response_data = response.json()
+        print("Performing request to Weather API for city Paris...")
+        print(
+            f"Paris/France {response_data["location"]["localtime"]} "
+            f"Weather: {response_data["current"]["temp_c"]} Celsius, "
+            f"{response_data["current"]["condition"]["text"]}"
+        )
+    else:
+        print(f"{response.status_code}", end=" ")
+        error = response.json().get("error")
+        if error:
+            print(error["message"])
 
 
 if __name__ == "__main__":
