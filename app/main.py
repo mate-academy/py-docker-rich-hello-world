d7b53cb8f013708f475ae52b4d43e7f42d13b402
Olena Zaritska <zaritskaos@gmail.com>
2024-10-06 20:14:39 +0300

    fix flake8

--- app/main.py
+++ app/main.py
@@ -10,11 +10,13 @@ def get_weather() -> None:
     response = requests.get(URL, params=params)
     if response.status_code == 200:
         response_data = response.json()
+        localtime = response_data["location"]["localtime"]
+        temp_c = response_data["current"]["temp_c"]
+        condition = response_data["current"]["condition"]["text"]
         print("Performing request to Weather API for city Paris...")
         print(
-            f"Paris/France {response_data["location"]["localtime"]} "
-            f"Weather: {response_data["current"]["temp_c"]} Celsius, "
-            f"{response_data["current"]["condition"]["text"]}"
+            f"Paris/France {localtime} "
+            f"Weather: {temp_c} Celsius, {condition}"
         )
     else:
         print(f"{response.status_code}", end=" ")
