27b736da1b0d4c6f9ec2dadcecedead870e29928
Stepan <oleksiukstepann@gmail.com>
2024-10-08 20:30:40 +0300

    add blank line & make try block smaller

--- app/main.py
+++ app/main.py
@@ -4,6 +4,7 @@ from datetime import datetime
 import requests
 from dotenv import load_dotenv
 
+
 load_dotenv()
 
 API_KEY = os.environ.get("API_KEY")
@@ -12,29 +13,29 @@ CITY = "Paris"
 
 
 def get_weather() -> None:
-    try:
-        full_url = f"{API_URL}{CITY}&aqi=no"
+    full_url = f"{API_URL}{CITY}&aqi=no"
+    weather_data = {}
 
+    try:
         response = requests.get(full_url)
         weather_data = response.json()
-
-        location = weather_data["location"]["name"]
-        country = weather_data["location"]["country"]
-        temp_c = weather_data["current"]["temp_c"]
-        condition = weather_data["current"]["condition"]["text"]
-        localtime = weather_data["location"]["localtime"]
-
-        localtime = datetime.strptime(localtime, "%Y-%m-%d %H:%M")
-        formatted_time = localtime.strftime("%Y-%m-%d %H:%M")
-
-        print(f"Performing request to Weather API for city {location}...")
-        print(
-            f"{location}/{country} {formatted_time} "
-            f"Weather: {temp_c}° Celsius, {condition}"
-        )
     except requests.exceptions.RequestException as e:
         print(f"Error fetching weather data: {e}")
 
+    location = weather_data["location"]["name"]
+    country = weather_data["location"]["country"]
+    temp_c = weather_data["current"]["temp_c"]
+    condition = weather_data["current"]["condition"]["text"]
+    localtime = weather_data["location"]["localtime"]
+
+    localtime = datetime.strptime(localtime, "%Y-%m-%d %H:%M")
+    formatted_time = localtime.strftime("%Y-%m-%d %H:%M")
+
+    print(f"Performing request to Weather API for city {location}...")
+    print(
+        f"{location}/{country} {formatted_time} "
+        f"Weather: {temp_c}° Celsius, {condition}"
+    )
 
 if __name__ == "__main__":
     get_weather()
