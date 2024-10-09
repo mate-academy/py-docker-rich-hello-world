45c28ee23bb8bd1edb5de1da7305eea3d92dad94
Denys Piuro <denys.pyuro@gmail.com>
2024-10-09 22:21:11 +0300

    Solut

--- app/main.py
+++ app/main.py
@@ -1,9 +1,16 @@
 import os
 import requests
+import logging
 
 
 WEATHER_API_URL = "http://api.weatherapi.com/v1/current.json"
 DEFAULT_CITY = "Paris"
+
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s - %(levelname)s - %(message)s"
+)
 
 
 def fetch_weather_data(api_key: str, city: str) -> dict:
@@ -14,7 +21,7 @@ def fetch_weather_data(api_key: str, city: str) -> dict:
         response.raise_for_status()
         return response.json()
     except requests.exceptions.RequestException as e:
-        print(f"Network error: {e}")
+        logging.error(f"Network error: {e}")
         return {}
 
 
@@ -25,16 +32,18 @@ def display_weather_info(data: dict) -> None:
         condition_text = data["current"]["condition"]["text"]
         location = data["location"]["name"]
         country = data["location"]["country"]
-        print(
+        logging.info(
             f"{location}/{country} {location_time} Weather: "
             f"{temperature} Celsius, {condition_text}"
         )
     else:
-        print("Failed to retrieve weather information.")
+        logging.warning("Failed to retrieve weather information.")
 
 
 def get_weather(api_key: str) -> None:
-    print(f"Performing request to Weather API for city {DEFAULT_CITY}...")
+    logging.info(
+        f"Performing request to Weather API for city {DEFAULT_CITY}..."
+    )
     data = fetch_weather_data(api_key, DEFAULT_CITY)
     display_weather_info(data)
 
@@ -44,4 +53,6 @@ if __name__ == "__main__":
     if api_key:
         get_weather(api_key)
     else:
-        print("API_KEY not found. Please set the environment variable.")
+        logging.error(
+            "API_KEY not found. Please set the environment variable."
+        )
