333bad7ac062517b765afa3da684d983335bed94
Maxim Turenko <turenkomaksim099@gmail.com>
2024-10-09 20:45:47 +0300

    implement py-docker-weather-api

--- app/main.py
+++ app/main.py
@@ -1,6 +1,27 @@
+import os
+from datetime import datetime as dt
+
+from dotenv import load_dotenv
+import requests
+
+load_dotenv()
+BASE_URL = "https://api.weatherapi.com/v1/current.json?"
+CITY = "Paris"
+
+
 def get_weather() -> None:
-    # write your code here
-    pass
+    API_KEY = os.getenv('API_KEY')
+    if not API_KEY:
+        raise ValueError("No API key found in environment variables")
+
+    url = BASE_URL + f"key={API_KEY}&q={CITY}"
+    response = requests.get(url).json()
+    current_time = dt.now().strftime('%Y-%m-%d %H:%M')
+
+    location = f"{response['location']['name']}/{response['location']['country']}"
+    weather = f"Weather: {response['current']['temp_c']} Celsius {response['current']['condition']['text']}"
+
+    print(f"{location} {current_time} {weather}")
 
 
 if __name__ == "__main__":
