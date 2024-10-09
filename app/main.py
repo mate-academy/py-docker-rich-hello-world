d8344ab75393d3e6cfd16c74b3fd25c102f3c43b
Dmytro Poznanskyi <poznanskyi.d@gmail.com>
2024-10-09 20:33:49 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,6 +1,23 @@
+import os
+
+import requests
+
+
+API_KEY = os.environ.get("API_KEY")
+
+API_URL = f"https://api.weatherapi.com/v1/current.json?key={API_KEY}&q="
+
+CITY = "Paris"
+
+
 def get_weather() -> None:
-    # write your code here
-    pass
+    print(f"Getting weather for {CITY}")
+    response = requests.get(f"{API_URL}?q={CITY}&key={API_KEY}")
+    if response.status_code == 200:
+        data = response.json()
+        print(f"Weather: {data}")
+    else:
+        print(f"Error: {response.status_code}")
 
 
 if __name__ == "__main__":
