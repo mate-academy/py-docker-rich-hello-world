8a377775d9463f9fca833d89a3e216422e328a93
Daniil <daniilrizhenko@gmail.com>
2024-10-10 19:50:03 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,6 +1,23 @@
+import requests
+from pprint import pprint
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
 def get_weather() -> None:
-    # write your code here
-    pass
+    api_key = os.getenv("API_KEY")
+
+    url = "http://api.weatherapi.com/v1/current.json"
+    params = {
+        "key": api_key,
+        "q": "Paris"
+    }
+
+    response = requests.get(url, params=params)
+    weather_data = response.json()
+    pprint(weather_data)
 
 
 if __name__ == "__main__":
