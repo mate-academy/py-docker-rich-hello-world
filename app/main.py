ff71a119450949e7798b9fd2699da55afb0453a2
oleksashcherbakov <oleksa.shcherbakov@gmail.com>
2024-10-14 11:20:14 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -7,11 +7,11 @@ load_dotenv()
 KEY = os.getenv("API_KEY")
 URL = "http://api.weatherapi.com/v1/current.json"
 LOCATION = os.getenv("city")
+CURRENT_PARAMS = {"q": LOCATION, "key": KEY}
 
 
 def get_weather() -> None:
-    current_param = {"q": LOCATION, "key": KEY}
-    response = requests.get(URL, params=current_param).json()
+    response = requests.get(URL, params=CURRENT_PARAMS).json()
 
     print(f"Current weather in {LOCATION} is: "
           f"{response['current']['temp_c']} C, "
