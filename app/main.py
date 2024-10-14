31fcc09209a532f56cde1a5638f6acbb140acb0b
Illia <ilaruslanovich7@gmail.com>
2024-10-14 17:03:56 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,19 +1,20 @@
 import requests
 import os
 
+
 def get_weather(api_key: str) -> None:
-    CITY = "Paris"
-    BASE_URL = f"http://api.weatherapi.com/v1/current.json?key={api_key}&q={CITY}"
-    response = requests.get(BASE_URL)
+    city = "Paris"
+    base_url = f"http://api.weatherapi.com/v1/current.json?key=" f"{api_key}&q={city}"
+    response = requests.get(base_url)
     weather = response.json()
 
     if response.status_code == 200:
         print(
-            f"{weather['location']['name']}/"
-            f"{weather['location']['country']} "
-            f"{weather['location']['localtime']} "
-            f"Weather: {weather['current']['temp_c']} "
-            f"Celsius, {weather['current']['condition']['text']}"
+            f"{weather["location"]["name"]}/"
+            f"{weather["location"]["country"]} "
+            f"{weather["location"]["localtime"]} "
+            f"Weather: {weather["current"]["temp_c"]} "
+            f"Celsius, {weather["current"]["condition"]["text"]}"
         )
     else:
         "There is no data about weather"
