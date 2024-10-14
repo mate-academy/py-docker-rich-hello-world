9eda4870034da08f7d6c8744a517bbf09735ef3a
oleksashcherbakov <oleksa.shcherbakov@gmail.com>
2024-10-14 11:01:57 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,6 +1,21 @@
+import os
+import requests
+from dotenv import load_dotenv
+
+load_dotenv()
+
+key = os.getenv("API_KEY")
+
 def get_weather() -> None:
-    # write your code here
-    pass
+    response = requests.get(
+        "http://api.weatherapi.com/v1/current.json",
+        params={"q": "Paris", "key": key}
+    ).json()
+
+    print(f"Current weather in Paris is: "
+          f"{response['current']['temp_c']} C, "
+          f"humidity: {response['current']['humidity']} %, "
+          f"condition: {response['current']['condition']['text']}")
 
 
 if __name__ == "__main__":
