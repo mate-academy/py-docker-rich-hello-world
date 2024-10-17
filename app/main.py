a18991ce4e97e38c1e10ec6c7b62b27c5d9e09f0
Tsvetan Kichuk <toys4babyodua@gmail.com>
2024-10-17 08:00:00 +0200

    add dockerfile, dockerignore

--- app/main.py
+++ app/main.py
@@ -1,7 +1,26 @@
-def get_weather() -> None:
-    # write your code here
-    pass
+import requests
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+api_key = os.getenv("API_KEY")
+print(api_key)
+
+URL = "http://api.weatherapi.com/v1"
+CITY = "Paris"
+
+
+def get_weather(CITY, api_key):
+    complete_url = f"{URL}/current.json?key={api_key}&q={CITY}"
+    response = requests.get(complete_url)
+    data = response.json()
+    return data
 
 
 if __name__ == "__main__":
-    get_weather()
+    weather_data = get_weather(CITY, api_key)
+
+    print(f"Weather in: {CITY}")
+    print(f"Temperature: {weather_data["current"]["temp_c"]} °C")
+    print(f"Condition: {weather_data["current"]["condition"]["text"]}")
