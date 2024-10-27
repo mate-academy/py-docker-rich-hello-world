7cbb2530464ad4e36a5d79d0cf6ebe9b73684d66
struhanets <posseydon87@gmail.com>
2024-10-27 12:29:07 +0100

    Solution

--- app/main.py
+++ app/main.py
@@ -7,10 +7,13 @@ load_dotenv()  # Завантажуємо змінні з .env
 
 API_KEY = os.getenv("API_KEY")
 URL = "https://api.weatherapi.com/v1/current.json"
-FILTERING = "Kyiv"
+FILTERING = "Paris"
 
 
 def get_weather() -> None:
+    if not API_KEY:
+        print("API KEY not set")
+
     params = {
         "key": API_KEY,
         "q": FILTERING
