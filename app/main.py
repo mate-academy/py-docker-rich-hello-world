71cc78836a7bb3d2ac3bcfcdb48c5a3d6a63950b
struhanets <posseydon87@gmail.com>
2024-10-27 12:21:54 +0100

    Solution

--- app/main.py
+++ app/main.py
@@ -1,6 +1,23 @@
+import os
+
+import requests
+from dotenv import load_dotenv
+
+load_dotenv()  # Завантажуємо змінні з .env
+
+API_KEY = os.getenv("API_KEY")
+URL = "https://api.weatherapi.com/v1/current.json"
+FILTERING = "Kyiv"
+
+
 def get_weather() -> None:
-    # write your code here
-    pass
+    params = {
+        "key": API_KEY,
+        "q": FILTERING
+    }
+    response = requests.get(URL, params=params)
+
+    print(response.json())
 
 
 if __name__ == "__main__":
