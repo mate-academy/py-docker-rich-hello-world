0ad01583c16708c04243f1aa0c0529e799958fb5
AndreiVed <catblues87@gmail.com>
2024-10-30 12:27:09 +0200

    create script in main.py, Dockerfile, .dockerignore

--- app/main.py
+++ app/main.py
@@ -1,6 +1,18 @@
+import requests
+
+FORMAT = "json"
+API_KEY = "1fc28b99320e4fe19b393031243010"
+URL = "https://api.weatherapi.com/v1/"
+FILTERING = "Paris"
+
+
+# https://api.weatherapi.com/v1/current.json?key=1fc28b99320e4fe19b393031243010&q=Paris
 def get_weather() -> None:
-    # write your code here
-    pass
+
+    response = requests.get(
+        URL + f"current.{FORMAT}?key={API_KEY}&q={FILTERING}"
+    )
+    print(response.status_code, response.json())
 
 
 if __name__ == "__main__":
