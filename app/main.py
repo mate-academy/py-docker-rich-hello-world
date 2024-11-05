09a13b65c11dd9df2cb57c4ad8da52b91a1b7c31
Denys Kibalenko <dennieking87@gmail.com>
2024-11-05 11:02:53 +0100

    Solution: add error handling for response and invalid data

--- app/main.py
+++ app/main.py
@@ -8,18 +8,32 @@ FILTERING = "Paris"
 
 
 def get_weather() -> None:
-    payload = {
-        "key": API_KEY,
-        "q": FILTERING,
-    }
-    res = requests.get(BASE_URL + "/current.json", params=payload)
-    data = res.json()
-    location = data["location"]
-    current = data["current"]
-    print("Performing request to Weather API for city Paris...")
-    print(f"{location['name']}/{location['country']} "
-          f"{location['localtime']} Weather: {current['temp_c']} Celsius, "
-          f"{current['condition']['text']}")
+    if API_KEY is not None:
+        payload = {
+            "key": API_KEY,
+            "q": FILTERING,
+        }
+        res = requests.get(BASE_URL + "/current.json", params=payload)
+        try:
+            res.raise_for_status()
+        except requests.exceptions.HTTPError as err:
+            print(f"Error: {err}")
+            return
+        data = res.json()
+        location = data.get("location")
+        current = data.get("current")
+        if location is not None or current is not None:
+            print("Performing request to Weather API for city Paris...")
+            print(f"{location['name']}/{location['country']} "
+                f"{location['localtime']} Weather: {current['temp_c']} "
+                f"Celsius, {current['condition']['text']}")
+        else:
+            print("Error: Invalid response from Weather API")
+            return
+    else:
+        print("Error: WEATHER_API_KEY is not set. \
+              Please set the environment variable and try again.")
+        return
 
 
 if __name__ == "__main__":
