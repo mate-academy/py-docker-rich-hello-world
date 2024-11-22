2cddf21a6869ed80d01edb45a9a9e687f3d10537
Maksym Protsak <112944888+MaksymProtsak@users.noreply.github.com>
2024-11-22 08:16:13 +0200

    Filled get_weather function.

--- app/main.py
+++ app/main.py
@@ -1,7 +1,21 @@
-def get_weather() -> None:
-    # write your code here
-    pass
+import requests
+
+
+def get_weather() -> str:
+    response = requests.get(
+        url="https://api.weatherapi.com/v1/current.json",
+        params={"q": "Paris", "key": "8ba46685a31a4765b9455446242211"}
+    )
+    r_json = response.json()
+    r_str = (
+        f"{r_json["location"]["name"]}"
+        f"/{r_json["location"]["country"]} "
+        f"{r_json["location"]["localtime"]} "
+        f"Weather: {r_json["current"]["temp_c"]} Celsius, "
+        f"{r_json["current"]["condition"]["text"]}"
+    )
+    return r_str
 
 
 if __name__ == "__main__":
-    get_weather()
+    print(get_weather())
