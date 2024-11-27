66bff154b53324ad95eea60e494b568fa8c05b6a
Maksym Bondarevskyi <bondarevskiymaks@gmail.com>
2024-11-27 18:46:00 +0200

    Implement docker Weather API

--- app/main.py
+++ app/main.py
@@ -1,6 +1,35 @@
+import os
+
+import requests
+from dotenv import load_dotenv
+
+load_dotenv()
+
+API_KEY = os.getenv("API_KEY")
+
+if not API_KEY:
+    raise ValueError("API_KEY is not found.")
+
+BASE_URL = "http://api.weatherapi.com/v1/current.json"
+CITY = "Paris"
+
+
 def get_weather() -> None:
-    # write your code here
-    pass
+    params = {
+        "key": API_KEY,
+        "q": CITY
+    }
+    res = requests.get(BASE_URL, params=params)
+    if res.status_code == 200:
+        data = res.json()
+        print(f"Taking weather information about {CITY} from Weather API")
+        print(
+            f"Location: {data["location"]["name"]}, {data["location"]["country"]}\n"
+            f"Time: {data["location"]["localtime"]}\n"
+            f"Temperature: {data["current"]["temp_c"]} Celsius degrees"
+        )
+    else:
+        print("Error:", res.status_code, res.text)
 
 
 if __name__ == "__main__":
