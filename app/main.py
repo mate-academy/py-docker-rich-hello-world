d74a09fc228531dfddd801f2717a0f690b261127
Viesich <v.s.vesich@gmail.com>
2024-11-27 09:52:40 +0200

    create script

--- app/main.py
+++ app/main.py
@@ -1,6 +1,38 @@
+import os
+
+from dotenv import load_dotenv
+
+import requests
+
+
+load_dotenv()
+
+
 def get_weather() -> None:
-    # write your code here
-    pass
+    api_key = os.getenv("API_KEY")
+    if not api_key:
+        raise ValueError("API_KEY environment variable is not set")
+
+    city = "Paris"
+    base_url = "http://api.weatherapi.com/v1/current.json"
+
+    url = f"{base_url}?key={api_key}&q={city}"
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        weather_data = response.json()
+        current_weather = weather_data.get("current", {})
+
+        if current_weather:
+            temperature = current_weather.get("temp_c")
+            condition = current_weather.get('condition', {}).get('text')
+            print(f"Weather in {city}:")
+            print(f"Temperature: {temperature}°C")
+            print(f"Weather condition: {condition}")
+        else:
+            print("Failed to retrieve weather data.")
+    else:
+        print(f"Request error: {response.status_code}")
 
 
 if __name__ == "__main__":
