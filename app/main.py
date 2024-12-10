3af97f349912f290eec866ef0711e5c0a59f2cb4
Mykola <jacktyler2391@gmail.com>
2024-12-10 16:52:13 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,6 +1,40 @@
+import requests
+
+import os
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
 def get_weather() -> None:
-    # write your code here
-    pass
+    params = {
+        "key": os.getenv("WEATHER_API_KEY"),
+    }
+
+    get_weather_in_paris_url = (
+        "http://api.weatherapi.com/v1//"
+        "current.json?q=Paris"
+    )
+
+    response = requests.get(get_weather_in_paris_url, params=params)
+    weather = response.json()
+
+    if weather:
+        location = weather.get("location")
+        current = weather.get("current", {})
+        print(
+            f"Country: "
+            f"{location["country"]}"
+            f" - City: {location["name"]}"
+            f" - Region: {location["region"]} | "
+            f" {location["localtime"]}"
+        )
+        print(f"Temperature: "
+              f" {current["temp_c"]}"
+              f" celsius")
+    else:
+        print("Weather is currently unavailable")
 
 
 if __name__ == "__main__":
