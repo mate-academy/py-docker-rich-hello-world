f3d53384206e7f1c283729ae78948362db0be51d
Mykola <jacktyler2391@gmail.com>
2024-12-10 17:11:52 +0200

    fix code style try 2

--- app/main.py
+++ app/main.py
@@ -12,9 +12,8 @@ def get_weather() -> None:
         "key": os.getenv("WEATHER_API_KEY"),
     }
 
-    get_weather_in_paris_url = (
-        "http://api.weatherapi.com/v1/current.json?q=Paris"
-    )
+    get_weather_in_paris_url = ("http://api.weatherapi.com/v1/"
+                                "current.json?q=Paris")
 
     response = requests.get(get_weather_in_paris_url, params=params)
     weather = response.json()
@@ -22,17 +21,12 @@ def get_weather() -> None:
     if weather:
         location = weather.get("location")
         current = weather.get("current", {})
-        print(
-            f"Country: {location["country"]} "
-            f" - City: {location["name"]} "
-            f" - Region: {location["region"]} "
-            f" {location["localtime"]} "
-        )
-        print(
-            f"Temperature: "
-            f" {current["temp_c"]}"
-            f" celsius"
-        )
+        print(f"Country: {location["country"]}")
+        print(f"City: {location["name"]}")
+        print(f"Region: {location["region"]}")
+        print(f"{location["localtime"]}")
+        print(f"Temperature: {current["temp_c"]} celsius")
+
     else:
         print("Weather is currently unavailable")
 
