a10ba4d1f9fff11548b271bbda981eb74309d1ff
Olevksy Ivan <kiperdemiwan@gmail.com>
2024-12-11 16:19:53 +0200

    solution1

--- app/main.py
+++ app/main.py
@@ -4,19 +4,17 @@ import os
 API_KEY = os.getenv("API_KEY")
 city = "Paris"
 
-
 def get_weather() -> None:
-    url = (
-        f"http:"
-        f"//api.weatherapi.com/v1/current.json?key={API_KEY}"
-        f"&q={city}&aqi=no"
-    )
-    response = requests.get(url)
-
-    if response.status_code == 200:
+    url = f"http://api.weatherapi.com/v1/current.json?key={API_KEY}&q={city}&aqi=no"
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
         data = response.json()
         print(f"Weather in {city}: {data['current']['temp_c']}°C")
-
+    except requests.exceptions.RequestException as e:
+        print(f"Error fetching weather data: {e}")
+    except KeyError:
+        print("Unexpected response format.")
 
 if __name__ == "__main__":
     get_weather()
