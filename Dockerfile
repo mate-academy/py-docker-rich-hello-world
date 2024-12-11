725130126276a50b92afe638a8dfd2cf9a46df5c
Serhii Berdianskyi <zeferot94@gmail.com>
2024-12-11 14:19:00 +0200

    Solution

---
+++ Dockerfile
@@ -0,0 +1,12 @@
+FROM python:3.12.8-alpine
+LABEL mainteiner="sberdianskyi@mate.com"
+ENV PYTHONBUFFERED=1
+
+WORKDIR /app
+
+COPY requirements.txt requirements.txt
+RUN pip install -r requirements.txt
+
+COPY . .
+
+CMD ["python", "app/main.py"]
--- app/main.py
+++ app/main.py
@@ -1,6 +1,22 @@
+import json
+import os
+from dotenv import load_dotenv
+
+import requests
+
+load_dotenv()
+
+BASE_URL = "https://api.weatherapi.com/v1/current.json?"
+API_KEY = os.environ["API_KEY"]
+CITY = "Paris"
+
+
 def get_weather() -> None:
-    # write your code here
-    pass
+    url = BASE_URL + f"key={API_KEY}&q={CITY}"
+    res = requests.get(url)
+    data = res.json()
+    formatted_data = json.dumps(data, ensure_ascii=False, indent=4)
+    print(formatted_data)
 
 
 if __name__ == "__main__":
