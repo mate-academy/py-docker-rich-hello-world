23c8e3864fc45193e876fb086dd5f35335a62eb9
lev2099 <lev2099@gmail.com>
2024-12-15 02:53:07 +0200

    Add weather API integration and Dockerize the application
    Integrated a weather API call to fetch current weather details for a specific location. Added a `.dockerignore` file and a `Dockerfile` to containerize the app. Updated `requirements.txt` to include necessary dependencies for the API request.

--- app/main.py
+++ app/main.py
@@ -1,6 +1,32 @@
+import os
+
+import requests
+
+
+URL = "https://api.weatherapi.com/v1/current.json?"
+
+FILTERING = "Ile-de-France"
+
+
 def get_weather() -> None:
-    # write your code here
-    pass
+
+    api_key = os.getenv("API_KEY")
+
+    if not api_key:
+        print("Error: API_KEY is not set in environment variables")
+        return
+
+    response = requests.get(
+        URL + "key=" + api_key + "&q=" + FILTERING + "&aqi=yes"
+    )
+    if response.status_code == 200:
+        data = response.json()
+        print(f"{data["location"]["name"]}/{data["location"]["country"]} "
+              f"{data["current"]["last_updated"]} "
+              f"Weather: {data["current"]["temp_c"]} Celsius, "
+              f"{data["current"]["condition"]["text"]}")
+    else:
+        print(f"Error: {response.status_code}")
 
 
 if __name__ == "__main__":
