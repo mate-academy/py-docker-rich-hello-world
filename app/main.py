82a317051dfecfb9adfd00ae9d72497d8206ccb8
Max <mxbely98@gmail.com>
2024-12-15 18:14:14 +0200

    implement get wether api for paris and builded docker

--- app/main.py
+++ app/main.py
@@ -1,7 +1,24 @@
-def get_weather() -> None:
-    # write your code here
-    pass
+import requests
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+
+def get_weather() -> dict:
+    URL = "http://api.weatherapi.com/v1/current.json"
+    CITY = "Paris"
+    res = requests.get(f"{URL}?key={os.getenv("API_KEY")}&q={CITY}")
+    city = res.json()["location"]["name"]
+    country = res.json()["location"]["country"]
+    date = res.json()["location"]["localtime"]
+    temp_c = res.json()["current"]["temp_c"]
+    if res.json()["current"]["cloud"] > 50:
+        weather = "Cloudy"
+    else:
+        weather = "Sunny"
+    return f"{city}/{country} {date}, Wether: {temp_c} Celsius, {weather}"
 
 
 if __name__ == "__main__":
-    get_weather()
+    print(get_weather())
