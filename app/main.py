a109432c543e04d856c4571723b894c4e04dbf3a
mregorlite <egor.infinity@gmail.com>
2024-12-18 20:37:25 +0200

    fix flake8

--- app/main.py
+++ app/main.py
@@ -4,13 +4,15 @@ import dotenv
 import requests
 
 
+CITY = "Paris"
+URL = "http://api.weatherapi.com/v1/current.json"
+
+
 def get_weather() -> None:
     key = os.getenv("API_KEY")
     if not key:
         print("No API key provided")
         return
-    CITY = "Paris"
-    URL = "http://api.weatherapi.com/v1/current.json"
     request = requests.get(
         URL,
         params={"key": key, "q": CITY},
