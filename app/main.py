fcc1c20a66ab3664424e13746c7886df00900c14
user <nikitaskrypets@gmail.com>
2024-12-18 17:51:16 +0200

    better constraint names

--- app/main.py
+++ app/main.py
@@ -4,15 +4,15 @@ import os
 from dotenv import load_dotenv
 
 load_dotenv()
+API_KEY = os.getenv("API_KEY") # noqa
+FILTERING = os.getenv("FILTERING") # noqa
 
 
 def get_weather() -> None:
-    api_key = os.getenv("API_KEY")
-    filtering = os.getenv("FILTERING")
-    print(f"Performing request to Weather API for city {filtering}...")
+    print(f"Performing request to Weather API for city {FILTERING}...")
     res = requests.get(
         f"https://api.weatherapi.com/v1/"
-        f"current.json?key={api_key}&q={filtering}"
+        f"current.json?key={API_KEY}&q={FILTERING}"
         f"&aqi=no"
     )
     res = res.json()
