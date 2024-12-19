bf00e0f4b8791f1f753d00f3dead873194443936
Alex <alex.gal.trot@gmail.com>
2024-12-19 19:07:34 +0100

    add new verifies

--- app/main.py
+++ app/main.py
@@ -13,20 +13,23 @@ PARAMS = {
 
 
 def get_res() -> dict:
+    if not API_KEY:
+        raise ValueError("API key is not set!")
     try:
         print("Performing request to Weather API to city Paris...")
         response = requests.get(URL, params=PARAMS)
         response.raise_for_status()
+        return response.json()
     except requests.exceptions.HTTPError as err:
         print(f"HTTP ERROR: {err}")
     except requests.exceptions.RequestException as err:
         print(f"REQUEST ERROR: {err}")
-    return response.json()
 
 
 def get_weather() -> None:
     weather = get_res()
-
+    if not weather:
+        raise ValueError("Data not received!")
     location = weather["location"]["name"]
     country = weather["location"]["country"]
     temperature = weather["current"]["temp_c"]
