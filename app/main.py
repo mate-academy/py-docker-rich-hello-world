c8ccc2ad5d39101bbf68e4ca2f05727e38f0c5ae
Vadym Sulim <ohnooq1@gmail.com>
2024-12-19 02:10:31 +0200

    implemented main logic

--- app/main.py
+++ app/main.py
@@ -1,7 +1,23 @@
-def get_weather() -> None:
-    # write your code here
-    pass
+import requests
+import os
+
+API_KEY = os.getenv("API_KEY")
+URL = "http://api.weatherapi.com/v1/current.json?"
+CITY = "Paris"
+
+
+def get_weather(city: str, key: str) -> None:
+    try:
+        response = requests.get(f"{URL}key={API_KEY}&q={CITY}&aqi=no")
+        response.raise_for_status()
+        data = response.json()
+        print(f"{data["location"]["name"]}/{data["location"]["country"]} "
+              f"{data["current"]["last_updated"]} "
+              f"Weather: {data["current"]["temp_c"]} Celsius, "
+              f"{data["current"]["condition"]["text"]}")
+    except requests.exceptions.RequestException as e:
+        print(f"Error: {e}")
 
 
 if __name__ == "__main__":
-    get_weather()
+    get_weather(CITY, API_KEY)
