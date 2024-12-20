1de9de530d581731b675b15677135709a1caac23
Den_k0 <denys.kotovskiy@gmail.com>
2024-12-20 10:41:57 +0200

    Solution fix: Constants Usage, Error Handling, Dockerfile improvements

--- app/main.py
+++ app/main.py
@@ -2,15 +2,17 @@ import os
 import requests
 from dotenv import load_dotenv
 
+load_dotenv()
+
+API_KEY = os.getenv("API_KEY")
+URL = f"http://api.weatherapi.com/v1/current.json"
+FILTERING = "Paris"
+
 
 def get_weather() -> None:
-    load_dotenv()
-    api_key = os.getenv("API_KEY")
-    url = f"http://api.weatherapi.com/v1/current.json?key={api_key}"
-    filtering = "Paris"
-
-    print(f"Performing request to Weather API for city {filtering}...")
-    response = requests.get(url + f"&q={filtering}")
+    print(f"Performing request to Weather API for city {FILTERING}...")
+    response = requests.get(URL + f"?key={API_KEY}&q={FILTERING}")
+    response.raise_for_status()
     data = response.json()
 
     print(
