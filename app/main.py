334de243c27866937c3f0989fa76440f47f4ff86
Roman Vynnytskyi <roman.vynnytskyi.tt@gmail.com>
2024-12-20 23:17:10 +0200

    fix flake8 requires

--- app/main.py
+++ app/main.py
@@ -8,13 +8,15 @@ load_dotenv()
 BASE_URL = "https://api.weatherapi.com/v1/current.json?"
 API_KEY = os.getenv("API_KEY")
 CITY = "PARIS"
-print(API_KEY)
+
+
 def get_weather() -> None:
 
     url = BASE_URL + f"q={CITY}&key={API_KEY}"
     response = requests.get(url).json()
     formatted_json = json.dumps(response, ensure_ascii=False, indent=4)
     print(formatted_json)
+
 
 if __name__ == "__main__":
     get_weather()
