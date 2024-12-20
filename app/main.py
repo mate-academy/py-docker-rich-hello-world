526c67597ccbde2fba40237e864399be7f2dbb8a
Volodymyr <uuforreasonsunknownuu@gmail.com>
2024-12-20 17:14:03 +0200

    flake

--- app/main.py
+++ app/main.py
@@ -4,11 +4,13 @@ import requests
 API_KEY = os.getenv("API_KEY")
 URL = "http://api.weatherapi.com/v1/current.json"
 
+
 def get_weather() -> None:
     response = requests.get(URL, params={"key": API_KEY, "q": "Paris", "aqi": "no"})
     response.raise_for_status()
     data = response.json()
     print(data)
+
 
 if __name__ == "__main__":
     get_weather()
