45a974b4da33801b0a1f8300ae3462065a8d9470
Den_k0 <denys.kotovskiy@gmail.com>
2024-12-20 12:51:52 +0200

    Solution fix

--- app/main.py
+++ app/main.py
@@ -1,27 +1,31 @@
 import os
+
 import requests
 from dotenv import load_dotenv
 
 load_dotenv()
 
-API_KEY = os.getenv("API_KEY")
+API_KEY = os.environ["API_KEY"]
 URL = "http://api.weatherapi.com/v1/current.json"
 FILTERING = "Paris"
 
 
 def get_weather() -> None:
-    print(f"Performing request to Weather API for city {FILTERING}...")
-    response = requests.get(URL + f"?key={API_KEY}&q={FILTERING}")
-    response.raise_for_status()
-    data = response.json()
+    try:
+        print(f"Performing request to Weather API for city {FILTERING}...")
+        response = requests.get(URL, params={"key": API_KEY, "q": FILTERING})
+        response.raise_for_status()
+        data = response.json()
 
-    print(
-        f"{data['location']['name']}/"
-        f"{data['location']['country']} "
-        f"{data['location']['localtime']} "
-        f"Weather: {data['current']['temp_c']} Celsius, "
-        f"{data['current']['condition']['text']}"
-    )
+        print(
+            f"{data['location']['name']}/"
+            f"{data['location']['country']} "
+            f"{data['location']['localtime']} "
+            f"Weather: {data['current']['temp_c']} Celsius, "
+            f"{data['current']['condition']['text']}"
+        )
+    except requests.exceptions.RequestException as e:
+        print(f"An error occurred: {e}")
 
 
 if __name__ == "__main__":
