137ecf0cd934acaf14f97031d7ca415d02820766
Yaroslav Bordovoy <yaroslavbordovoy@gmail.com>
2024-12-21 21:10:46 +0200

    Solution v2

--- app/main.py
+++ app/main.py
@@ -1,43 +1,42 @@
 import os
 
 import requests
-from requests import RequestException, Response
+from requests import Response
 
-URL = "http://api.weatherapi.com/v1/current.json"
+
+URL = os.environ.get("URL")
 API_KEY = os.environ.get("API_KEY")
-FILTERING = "Paris"
+FILTERING = os.environ.get("FILTERING")
 
 
 def get_response() -> Response:
     try:
         response = requests.get(URL + f"?key={API_KEY}&q={FILTERING}")
-    except ConnectionError:
-        raise ConnectionError(
-            "There was a problem connecting to the resource."
-        )
-    except RequestException:
-        raise RequestException("An error occurred while making the request.")
-
-    print(type(response))
+        response.raise_for_status()
+    except requests.ConnectionError:
+        raise RuntimeError("There was a problem connecting to the resource.")
+    except requests.RequestException:
+        raise RuntimeError("An error occurred while making the request.")
 
     return response
 
 
 def get_weather() -> None:
     response = get_response()
-    if response.status_code == 200:
+
+    if response.ok:
         try:
-            city = response.json().get("location").get("name")
-            country = response.json().get("location").get("country")
-            localtime = response.json().get("location").get("localtime")
-            celsius = response.json().get("current").get("temp_c")
-            condition = response.json().get(
-                "current"
-            ).get("condition").get("text")
-        except ValueError:
-            raise ValueError("Failed to decode the JSON response.")
-        except KeyError:
-            raise KeyError("Missing expected data in the response.")
+            location = response.json().get("location")
+            current_weather = response.json().get("current")
+
+            city = location.get("name")
+            country = location.get("country")
+            localtime = location.get("localtime")
+
+            celsius =current_weather.get("temp_c")
+            condition = current_weather.get("condition").get("text")
+        except (ValueError, KeyError):
+            raise ValueError("Failed to decode the JSON response or missing expected data.")
     else:
         print("Failed to retrieve required data.")
 
