778b0e299fbb68aa8d359dabf13f6087d87990a7
Oleksandr Zaiets <mykola.oleksandr.zaiets@gmail.com>
2024-12-23 02:55:15 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,7 +1,27 @@
-def get_weather() -> None:
-    # write your code here
-    pass
+import os
+import requests
+
+API_KEY = os.getenv("API_KEY")
+url = "http://api.weatherapi.com/v1/current.json"
 
 
+def get_weather(city):
+    params = {"key": API_KEY, "q": city}
+    response = requests.get(url, params=params)
+    if response.status_code == 200:
+        return response.json()
+    else:
+        return None
+
+
+# Main function
 if __name__ == "__main__":
-    get_weather()
+    city = "San Francisco"
+    weather_data = get_weather(city)
+    if weather_data:
+        print(
+            f"Weather in {city}: {weather_data['current']['condition']['text']}, "
+            f"Temperature: {weather_data['current']['temp_c']}°C"
+        )
+    else:
+        print("Failed to retrieve weather data.")
