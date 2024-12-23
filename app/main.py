f722a4281df7a8e84f083ee624966aae55d15a56
Oleksandr Zaiets <mykola.oleksandr.zaiets@gmail.com>
2024-12-23 02:58:39 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -5,7 +5,7 @@ API_KEY = os.getenv("API_KEY")
 url = "http://api.weatherapi.com/v1/current.json"
 
 
-def get_weather(city):
+def get_weather(city: str) -> dict | None:
     params = {"key": API_KEY, "q": city}
     response = requests.get(url, params=params)
     if response.status_code == 200:
@@ -20,7 +20,7 @@ if __name__ == "__main__":
     weather_data = get_weather(city)
     if weather_data:
         print(
-            f"Weather in {city}: {weather_data['current']['condition']['text']}, "
+            f"{city}: {weather_data['current']['condition']['text']}, "
             f"Temperature: {weather_data['current']['temp_c']}°C"
         )
     else:
