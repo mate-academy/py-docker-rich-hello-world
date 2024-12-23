7f67c649ab6268a1987e33380a508a257686aea9
Rabb1t-Artem <79219648+Rabb1t-Artem@users.noreply.github.com>
2024-12-23 08:45:41 +0200

    fix comment

--- app/main.py
+++ app/main.py
@@ -3,7 +3,7 @@ import os
 import requests
 
 
-#-----Constants----------------------------------------------------------------
+# -----Constants---------------------------------------------------------------
 API_KEY = os.getenv("API_KEY")
 URL = "https://api.weatherapi.com/v1/current.json?"
 FILTERING = "Paris"
