bb5ef45d2bbdcc1680339cf4e7f65887dd8064dc
Frezworx <frezworx@gmail.com>
2025-01-10 09:14:22 +0200

    Add weather API request functionality
    - Import `requests` module
    - Define constants for API URL, API key, and filtering location
    - Implement `get_weather` function to fetch and print current weather data

--- app/main.py
+++ app/main.py
@@ -1,6 +1,13 @@
+import requests
+
+URL = "https://api.weatherapi.com/v1/"
+API_KEY = "171e9f9e10f54ee4a5a144342250901"
+FILTERING = "London"
+
+
 def get_weather() -> None:
-    # write your code here
-    pass
+    response = requests.get(f"{URL}current.json?key={API_KEY}&q={FILTERING}")
+    print(response.json())
 
 
 if __name__ == "__main__":
