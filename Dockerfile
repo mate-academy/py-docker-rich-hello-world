9ae23d893a3c7a8f4e473110a93b4529912e081a
Oleksandr Zaiets <mykola.oleksandr.zaiets@gmail.com>
2025-01-23 01:01:17 +0200

    Solution

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,40 @@
-FROM python:3.9-slim
-WORKDIR /app
-COPY requirements.txt ./
-RUN pip install --no-cache-dir -r requirements.txt
-COPY app/ ./app
+# syntax=docker/dockerfile:1
+
+ARG PYTHON_VERSION=3.9
+FROM python:${PYTHON_VERSION}-slim as base
+
+# Prevents Python from writing pyc files
+ENV PYTHONDONTWRITEBYTECODE=1
+
+# Keeps Python from buffering stdout and stderr
+ENV PYTHONUNBUFFERED=1
+
+# Set API_KEY environment variable
 ENV API_KEY=""
+
+WORKDIR /app
+
+# Create a non-privileged user
+ARG UID=10001
+RUN adduser \
+    --disabled-password \
+    --gecos "" \
+    --home "/nonexistent" \
+    --shell "/sbin/nologin" \
+    --no-create-home \
+    --uid "${UID}" \
+    appuser
+
+# Install dependencies with cache optimization
+RUN --mount=type=cache,target=/root/.cache/pip \
+    --mount=type=bind,source=requirements.txt,target=requirements.txt \
+    python -m pip install --no-cache-dir -r requirements.txt
+
+# Copy only necessary files
+COPY app/ ./app
+
+# Switch to non-privileged user
+USER appuser
+
+# Run the application
 CMD ["python", "app/main.py"]
--- app/main.py
+++ app/main.py
@@ -2,25 +2,29 @@ import os
 import requests
 
 API_KEY = os.getenv("API_KEY")
+if not API_KEY:
+    raise KeyError("API_KEY not provided")
 url = "http://api.weatherapi.com/v1/current.json"
+CITY = "San Francisco"
 
 
 def get_weather(city: str) -> dict | None:
     params = {"key": API_KEY, "q": city}
-    response = requests.get(url, params=params)
-    if response.status_code == 200:
+    try:
+        response = requests.get(url, params=params)
+        response.raise_for_status()
         return response.json()
-    else:
+    except requests.exceptions.RequestException as e:
+        print(f"Error fetching weather data: {e}")
         return None
 
 
 # Main function
 if __name__ == "__main__":
-    city = "San Francisco"
-    weather_data = get_weather(city)
+    weather_data = get_weather(CITY)
     if weather_data:
         print(
-            f"{city}: {weather_data['current']['condition']['text']}, "
+            f"{CITY}: {weather_data['current']['condition']['text']}, "
             f"Temperature: {weather_data['current']['temp_c']}°C"
         )
     else:
