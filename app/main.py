bf705bd7ea64bd4cdfd887ac8b22be6cb99ba904
Anton Hryhorenko <anton.h.sind@gmail.com>
2025-01-25 23:37:24 +0100

    Solution

--- app/main.py
+++ app/main.py
@@ -4,7 +4,8 @@ import requests
 API_KEY = "c2a2981bc3344945b2c144432252301"
 BASE_URL = "https://api.weatherapi.com/v1/current.json"
 
-def get_weather():
+
+def get_weather() -> None:
     print("Performing request to Weather api for Paris...")
     url = f"{BASE_URL}?key={API_KEY}&q=Paris"
     try:
@@ -16,9 +17,16 @@ def get_weather():
             localtime = data["location"]["localtime"]
             temp_c = data["current"]["temp_c"]
             condition = data["current"]["condition"]["text"]
-            print(f"{city}/{country} {localtime} Weather: {temp_c} Celsius, {condition}")
+            print(
+                f"{city}/{country} "
+                f"{localtime} Weather: "
+                f"{temp_c} Celsius, {condition}"
+            )
         else:
-            print(f"Error: Unable to fetch weather data (status code: {response.status_code})")
+            print(
+                f"Error: Unable to fetch weather data "
+                f"(status code: {response.status_code})"
+            )
             return None
     except Exception as e:
         print(f"An error occurred: {e}")
